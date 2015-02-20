bc04234daa1dd871af5589e8f6a635f6c7ea3660
mikasoukhov <mika@stocksharp.com>
2015-02-20 22:43:52 +0300

    Hydra. Quandl

---
+++ Hydra/Plugins/Quandl/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Hydra.Quandl")]
+[assembly: AssemblyDescription("Hydra plugin for Quandl.")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d8d10d2c-0ac0-4e45-859b-f6b6b62e119d")]
