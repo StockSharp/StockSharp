7528f95906e0662a886825e1c077a1530662d23a
mikasoukhov <mika@stocksharp.com>
2015-02-28 16:42:08 +0300

    IDocService.PostNewVersion

---
+++ Community/DocClient.cs
@@ -0,0 +1,37 @@
+namespace StockSharp.Community
+{
+	using System;
+
+	/// <summary>
+	/// Клиент для доступа к <see cref="IDocService"/>.
+	/// </summary>
+	public class DocClient : BaseCommunityClient<IDocService>
+	{
+		/// <summary>
+		/// Создать <see cref="DocClient"/>.
+		/// </summary>
+		public DocClient()
+			: this(new Uri("http://stocksharp.com/services/docservice.svc"))
+		{
+		}
+
+		/// <summary>
+		/// Создать <see cref="DocClient"/>.
+		/// </summary>
+		/// <param name="address">Адрес сервера.</param>
+		public DocClient(Uri address)
+			: base(address, "doc", true)
+		{
+		}
+
+		/// <summary>
+		/// Загрузить описание новой версии.
+		/// </summary>
+		/// <param name="product">Тип продукта.</param>
+		/// <param name="description">Описание новой версии.</param>
+		public void PostNewVersion(Products product, string description)
+		{
+			Invoke(f => f.PostNewVersion(SessionId, product, description));
+		}
+	}
+}
--- Community/IDocService.cs
+++ Community/IDocService.cs
@@ -1,7 +1,45 @@
 ﻿namespace StockSharp.Community
 {
 	using System;
+	using System.Runtime.Serialization;
 	using System.ServiceModel;
+
+	/// <summary>
+	/// Продукты.
+	/// </summary>
+	[DataContract]
+	public enum Products
+	{
+		/// <summary>
+		/// S#.API
+		/// </summary>
+		[EnumMember]
+		Api,
+
+		/// <summary>
+		/// S#.Data
+		/// </summary>
+		[EnumMember]
+		Hydra,
+
+		/// <summary>
+		/// S#.Studio
+		/// </summary>
+		[EnumMember]
+		Studio,
+
+		/// <summary>
+		/// S#.Server
+		/// </summary>
+		[EnumMember]
+		Server,
+
+		/// <summary>
+		/// S#.StrategyRunner
+		/// </summary>
+		[EnumMember]
+		StrategyRunner
+	}
 
 	/// <summary>
 	/// Интерфейс, описывающий сервис документации.
@@ -32,5 +70,14 @@
 		/// <param name="pages">Новые страницы документации.</param>
 		[OperationContract]
 		void Upload(Guid sessionId, DocPage[] pages);
+
+		/// <summary>
+		/// Загрузить описание новой версии.
+		/// </summary>
+		/// <param name="sessionId">Идентификатор сессии.</param>
+		/// <param name="product">Тип продукта.</param>
+		/// <param name="description">Описание новой версии.</param>
+		[OperationContract]
+		void PostNewVersion(Guid sessionId, Products product, string description);
 	}
 }
