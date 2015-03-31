ec5c637a3e43f01afffbb1a79961558c3dad93f2
mikasoukhov <mika@stocksharp.com>
2015-03-31 19:42:36 +0300

    Samples. OrdersWindow. Move order ability.

--- Samples/SmartCom/SampleSmart/OrdersWindow.xaml.cs
+++ Samples/SmartCom/SampleSmart/OrdersWindow.xaml.cs
@@ -2,9 +2,15 @@
 {
 	using System.Collections.Generic;
 
+	using Ecng.Common;
+	using Ecng.Xaml;
+
 	using MoreLinq;
 
+	using StockSharp.Algo;
 	using StockSharp.BusinessEntities;
+	using StockSharp.Localization;
+	using StockSharp.Xaml;
 
 	public partial class OrdersWindow
 	{
@@ -16,6 +22,21 @@
 		private void OrderGrid_OnOrderCanceling(IEnumerable<Order> orders)
 		{
 			orders.ForEach(MainWindow.Instance.Trader.CancelOrder);
+		}
+
+		private void OrderGrid_OnOrderReRegistering(Order order)
+		{
+			var window = new OrderWindow
+			{
+				Title = LocalizedStrings.Str2976Params.Put(order.TransactionId),
+				Connector = MainWindow.Instance.Trader,
+				Order = order.ReRegisterClone(newVolume: order.Balance),
+			};
+
+			if (window.ShowModal(this))
+			{
+				MainWindow.Instance.Trader.ReRegisterOrder(order, window.Order);
+			}
 		}
 	}
 }
