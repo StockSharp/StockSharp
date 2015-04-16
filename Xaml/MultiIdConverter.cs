bac6569d425a8291ab2139a045f223f701a2e98a
mikasoukhov <mika@stocksharp.com>
2015-04-16 12:37:16 +0300

    OrderGrid. Show long and string identifiers.

--- Xaml/MultiIdConverter.cs
+++ Xaml/MultiIdConverter.cs
@@ -10,10 +10,18 @@
 	{
 		object IMultiValueConverter.Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
 		{
-			var londId = values[0].To<long?>();
+			var longId = values[0].To<long?>();
 			var strId = values[1].To<string>();
 
-			return londId == null ? strId : londId.To<string>();
+			var str = longId.To<string>();
+
+			if (str == null)
+				return strId;
+
+			if (strId != null)
+				return str + "/" + strId;
+
+			return str;
 		}
 
 		object[] IMultiValueConverter.ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
