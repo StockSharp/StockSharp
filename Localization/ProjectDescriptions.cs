df7317dd8a50dfec160d306a5e18b19e8352475f
mikasoukhov <mika@stocksharp.com>
2015-09-13 00:19:09 +0300

    Version 4.3.11

--- Localization/ProjectDescriptions.cs
+++ Localization/ProjectDescriptions.cs
@@ -28,6 +28,6 @@ namespace StockSharp.Localization
 		/// <summary>
 		/// Gets version information.
 		/// </summary>
-		public const string Version = "4.3.10";
+		public const string Version = "4.3.11";
 	}
 }
