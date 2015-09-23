38a775379e7db3a27deee04f1b4c679feb65ac49
Sergey-Dvortsov <dvortsov@list.ru>
2015-09-23 17:07:26 +0300

    WhrireHelpers change

--- Anywhere/Anywhere/WriteHelpers.cs
+++ Anywhere/Anywhere/WriteHelpers.cs
@@ -41,36 +41,6 @@
                 File.Create(_positionsFilePath);
         }
 
-        public static void WriteTrade(this Trade trade)
-        {
-            MemoryToFile(TradeToString(trade), _tradesFilePath);
-        }
-
-        public static void WriteMyTrade(this MyTrade trade)
-        {
-            MemoryToFile(MyTradeToString(trade), _myTradesFilePath);
-        }
-
-        public static void WriteMarketDepth(this MarketDepth depth)
-        {
-            DepthToFile(depth, Path.Combine(_outputFolder, string.Format("{0}_depth.txt", depth.Security.Code)));
-        }
-
-        public static void WriteLevel1(this Security security)
-        {
-            MemoryToFile(Level1ToString(security), _level1FilePath);
-        }
-
-        public static void WriteOrder(this Order order)
-        {
-            MemoryToFile(OrderToString(order), _ordersFilePath);
-        }
-
-        public static void WritePosition(this Position position)
-        {
-            MemoryToFile(PositionToString(position), _positionsFilePath);
-        }
-
         public static void WriteTrade(this ExecutionMessage tick)
         {
             MemoryToFile(TradeToString(tick), _tradesFilePath);
@@ -99,93 +69,6 @@
         public static void WritePosition(this PositionChangeMessage position)
         {
             MemoryToFile(PositionToString(position), _positionsFilePath);
-        }
-
-        // returns a string of trade
-        private static string TradeToString(Trade trade)
-        {
-            //securityId;tradeId;time;price;volume;orderdirection 
-            return string.Format("{0};{1};{2};{3};{4};{5}",
-                trade.Security.Id,
-                trade.Id,
-                trade.Time,
-                trade.Price,
-                trade.Volume,
-                trade.OrderDirection);
-        }
-
-        // returns a string of mytrade
-        private static string MyTradeToString(MyTrade trade)
-        {
-            //securityId;tradeId;time;volume;price;orderdirection;orderId 
-            return string.Format("{0};{1};{2};{3};{4};{5};{6}",
-                trade.Trade.Security.Id,
-                trade.Trade.Id,
-                trade.Trade.Time,
-                trade.Trade.Volume,
-                trade.Trade.Price,
-                trade.Trade.OrderDirection,
-                trade.Order.Id);
-        }
-
-        // returns a string of order
-        private static string OrderToString(Order order)
-        {
-            //orderId;transactionId;time;securityId;portfolioName;volume;balance;price;direction;type;localTime 
-            return string.Format("{0};{1};{2};{3};{4};{5};{6};{7};{8};{9};{10};{11}",
-                order.Id,
-                order.TransactionId,
-                order.Time,
-                order.Security.Id,
-                order.Portfolio.Name,
-                order.Volume,
-                order.Balance,
-                order.Price,
-                order.Direction,
-                order.Type,
-                order.State,
-                order.LocalTime);
-        }
-
-        // returns a string of position
-        private static string PositionToString(Position position)
-        {
-            return string.Format("{0};{1};{2};{3}",
-                position.Security.Id,
-                position.Portfolio.Name,
-                position.CurrentValue,
-                position.AveragePrice);
-        }
-
-        // returns a string of level1
-        private static string Level1ToString(Security security)
-        {
-            return string.Format("{0};{1};{2};{3};{4};{5};{6};{7};{8};{9};{10};{11};{12};{13}",
-                security.Id,
-                security.Board.Code,
-                security.PriceStep,
-                security.VolumeStep,
-                security.Type,
-                security.LastTrade.Price,
-                security.LastTrade.Volume,
-                security.LastTrade.Time.TimeOfDay,
-                security.LastTrade.Time.Date,
-                security.LastTrade.Time.Date,
-                security.BestBid.Price,
-                security.BestBid.Volume,
-                security.BestAsk.Price,
-                security.BestAsk.Volume
-                );
-        }
-
-        // returns a string of marketdepth quote
-        private static string QuoteToString(Quote quote)
-        {
-            return string.Format("{0};{1};{2}{3}",
-                quote.OrderDirection,
-                quote.Price,
-                quote.Volume,
-                Environment.NewLine);
         }
 
         private static string TradeToString(ExecutionMessage tick)
@@ -266,35 +149,6 @@
                 level.SecurityId.SecurityCode,
                 sb
                 );
-        }
-
-        // writing data to a file
-        private static void SaveToFile(string line, string filePath)
-        {
-            using (var file = new StreamWriter(filePath, true))
-                file.WriteLine(line);
-        }
-
-        // converts marketdepth to MemoryStream and writes to the file
-        private static void DepthToFile(MarketDepth depth, string filePath)
-        {
-            using (var mem = new MemoryStream(200))
-            {
-                for (var i = depth.Asks.GetUpperBound(0); i >= 0; i--)
-                {
-                    var bytes = Encoding.UTF8.GetBytes(QuoteToString(depth.Asks[i]));
-                    mem.Write(bytes, 0, bytes.Length);
-                }
-
-                for (var i = 0; i <= depth.Bids.GetUpperBound(0); i++)
-                {
-                    var bytes = Encoding.UTF8.GetBytes(QuoteToString(depth.Bids[i]));
-                    mem.Write(bytes, 0, bytes.Length);
-                }
-
-                using (var file = new FileStream(filePath, FileMode.Create, FileAccess.Write))
-                    mem.WriteTo(file);
-            }
         }
 
         private static void MemoryToFile(string line, string filePath)
