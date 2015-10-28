ddfe87c45294703c73afbe53ee5155b815aa8b52
mikasoukhov <mika@stocksharp.com>
2015-10-28 21:24:23 +0300

    AlertDiagramElement fix.

--- Samples/Common/SampleDiagram/App.xaml.cs
+++ Samples/Common/SampleDiagram/App.xaml.cs
@@ -1,15 +1,6 @@
 ﻿namespace SampleDiagram
 {
-	using Ecng.Configuration;
-
-	using StockSharp.Alerts;
-
 	public partial class App
 	{
-		public App()
-		{
-			//TODO remove after fix for loading elements from config
-			ConfigManager.RegisterService<IAlertService>(new AlertService(".\\"));
-		}
 	}
 }
