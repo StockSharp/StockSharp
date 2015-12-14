13dd96486cffbff97d507584c49c1dc956032c2d
jasonnator <undisclosed@email.com>
2015-12-14 14:39:51 -0500

    remove Algo Trading from copyright

--- Algo/Indicators/MoneyFlowIndex.cs
+++ Algo/Indicators/MoneyFlowIndex.cs
@@ -1,7 +1,7 @@
 #region S# License
 /******************************************************************************************
 NOTICE!!!  This program and source code is owned and licensed by
-StockSharp Algo Trading, LLC, www.stocksharp.com
+StockSharp, LLC, www.stocksharp.com
 Viewing or use of this code requires your acceptance of the license
 agreement found at https://github.com/StockSharp/StockSharp/blob/master/LICENSE
 Removal of this comment is a violation of the license agreement.
@@ -10,7 +10,7 @@ Project: StockSharp.Algo.Indicators.Algo
 File: MoneyFlowIndex.cs
 Created: 2015, 11, 23, 2:02 AM
 
-Copyright 2010 by StockSharp Algo Trading, LLC
+Copyright 2010 by StockSharp, LLC
 *******************************************************************************************/
 #endregion S# License
 namespace StockSharp.Algo.Indicators
--- Connectors/Rithmic/RithmicClient_Callbacks.cs
+++ Connectors/Rithmic/RithmicClient_Callbacks.cs
@@ -1,7 +1,7 @@
 #region S# License
 /******************************************************************************************
 NOTICE!!!  This program and source code is owned and licensed by
-StockSharp Algo Trading, LLC, www.stocksharp.com
+StockSharp, LLC, www.stocksharp.com
 Viewing or use of this code requires your acceptance of the license
 agreement found at https://github.com/StockSharp/StockSharp/blob/master/LICENSE
 Removal of this comment is a violation of the license agreement.
@@ -10,7 +10,7 @@ Project: StockSharp.Rithmic.Rithmic
 File: RithmicClient_Callbacks.cs
 Created: 2015, 12, 2, 8:18 PM
 
-Copyright 2010 by StockSharp Algo Trading, LLC
+Copyright 2010 by StockSharp, LLC
 *******************************************************************************************/
 #endregion S# License
 namespace StockSharp.Rithmic
