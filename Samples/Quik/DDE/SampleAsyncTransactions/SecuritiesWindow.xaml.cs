c5e75eaebaa739a2e7f257b488588277a172860f
Belich Aleksandr <a-belich@yandex.ru>
2016-05-02 22:57:21 +0600

    Build fix.

--- Samples/Quik/DDE/SampleAsyncTransactions/SecuritiesWindow.xaml.cs
+++ Samples/Quik/DDE/SampleAsyncTransactions/SecuritiesWindow.xaml.cs
@@ -113,7 +113,7 @@ namespace SampleAsyncTransactions
 				var wnd = _quotesWindows.TryGetValue(depth.Security);
 
 				if (wnd != null)
-					wnd.DepthCtrl.UpdateDepth(depth);
+					wnd.DepthCtrl.UpdatingDepth = depth;
 			}
 		}
 	}
