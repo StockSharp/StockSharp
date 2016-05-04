9a27dc58571dc9cfdac8600145dfe065e3fc647f
mika@stocksharp.com <mika@stocksharp.com>
2016-05-04 15:14:47 +0300

    LocalizedStrings_Dots.cs

---
+++ Localization/LocalizedStrings_Dots.cs
@@ -0,0 +1,160 @@
+namespace StockSharp.Localization
+{
+	partial class LocalizedStrings
+	{
+		/// <summary>
+		/// 
+		/// </summary>
+		public const string Dot = "Dot";
+
+		private static string AppendDot(string value)
+		{
+			return value + ".";
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public static string LineColorDot => AppendDot(LineColor);
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public static string FontColorDot => AppendDot(FontColor);
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public static string Timeframe2GridColorDot => AppendDot(Timeframe2GridColor);
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public static string Timeframe2FrameColorDot => AppendDot(Timeframe2FrameColor);
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public static string Timeframe3GridColorDot => AppendDot(Timeframe3GridColor);
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public static string MaxVolumeColorDot => AppendDot(MaxVolumeColor);
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public static string ClusterLineColorDot => AppendDot(ClusterLineColor);
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public static string ClusterTextColorDot => AppendDot(ClusterTextColor);
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public static string ClusterColorDot => AppendDot(ClusterColor);
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public static string ClusterMaxVolumeColorDot => AppendDot(ClusterMaxVolumeColor);
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public static string ShowHorizontalVolumesDot => AppendDot(ShowHorizontalVolumes);
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public static string LocalHorizontalVolumesDot => AppendDot(LocalHorizontalVolumes);
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public static string HorizontalVolumeWidthFractionDot => AppendDot(HorizontalVolumeWidthFraction);
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public static string HorizontalVolumeColorDot => AppendDot(HorizontalVolumeColor);
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public static string HorizontalVolumeFontColorDot => AppendDot(HorizontalVolumeFontColor);
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public static string Str2933Dot => AppendDot(Str2933);
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public static string BuyPendingColorDot => AppendDot(BuyPendingColor);
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public static string BuyColorDot => AppendDot(BuyColor);
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public static string BuyBlinkColorDot => AppendDot(BuyBlinkColor);
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public static string SellPendingColorDot => AppendDot(SellPendingColor);
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public static string SellColorDot => AppendDot(SellColor);
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public static string SellBlinkColorDot => AppendDot(SellBlinkColor);
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public static string CancelButtonColorDot => AppendDot(CancelButtonColor);
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public static string CancelButtonBgColorDot => AppendDot(CancelButtonBgColor);
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public static string AnimationDot => AppendDot(Animation);
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public static string Str1924Dot => AppendDot(Str1924);
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public static string Str1926Dot => AppendDot(Str1926);
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public static string Str1938Dot => AppendDot(Str1938);
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public static string LabelsFormatIntradayDescDot => AppendDot(LabelsFormatIntradayDesc);
+	}
+}
