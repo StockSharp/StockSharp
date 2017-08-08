72037a22a20e37f8d3da51e641b76c271e9d7eb3
StockSharp Dev1 <mika@stocksharp.com>
2017-08-09 00:54:14 +0300

    SampleQuantFeed.

---
+++ Samples/QuantHouse/SampleQuantFeed/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿#region S# License
+/******************************************************************************************
+NOTICE!!!  This program and source code is owned and licensed by
+StockSharp, LLC, www.stocksharp.com
+Viewing or use of this code requires your acceptance of the license
+agreement found at https://github.com/StockSharp/StockSharp/blob/master/LICENSE
+Removal of this comment is a violation of the license agreement.
+
+Project: %Namespace%.SampleOECPublic
+File: AssemblyInfo.cs
+Created: 2015, 11, 11, 2:32 PM
+
+Copyright 2010 by StockSharp, LLC
+*******************************************************************************************/
+#endregion S# License
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SampleQuantFeed")]
+[assembly: AssemblyDescription("")]
+
+[assembly: Guid("464AFB09-2474-46FB-937C-F33B2E76047E")]
+
+[assembly: ThemeInfo(
+	ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
+	//(used if a resource is not found in the page, 
+	// or application resource dictionaries)
+	ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
+	//(used if a resource is not found in the page, 
+	// app, or any theme specific resource dictionaries)
+)]
