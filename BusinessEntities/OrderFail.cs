7dc979d07a357da1179340df0dd07b32ce5e0dd5
StockSharp Dev1 <mika@stocksharp.com>
2018-01-03 18:33:32 +0300

    OrderFail. ToString override.

--- BusinessEntities/OrderFail.cs
+++ BusinessEntities/OrderFail.cs
@@ -72,5 +72,14 @@ namespace StockSharp.BusinessEntities
 			get => Order.ExtensionInfo;
 			set => Order.ExtensionInfo = value;
 		}
+
+		/// <summary>
+		/// Returns a string that represents the current object.
+		/// </summary>
+		/// <returns>A string that represents the current object.</returns>
+		public override string ToString()
+		{
+			return $"{Error?.Message}/{Order}";
+		}
 	}
 }
