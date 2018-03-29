0588a366262c56a176a542589f0e3c93f500c762
StockSharp Dev1 <mika@stocksharp.com>
2018-03-29 15:20:33 +0300

    FIX connector. Process unknown transactions option.

--- Localization/ProjectDescriptions.cs
+++ Localization/ProjectDescriptions.cs
@@ -43,6 +43,6 @@ namespace StockSharp.Localization
 		/// <summary>
 		/// Gets version information.
 		/// </summary>
-		public const string Version = "4.4.5.4";
+		public const string Version = "4.4.6";
 	}
 }
