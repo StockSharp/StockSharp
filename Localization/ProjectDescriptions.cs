85519e4c60610fba66ba5cd0dac32eac88d6fa89
StockSharp Dev1 <mika@stocksharp.com>
2018-12-01 19:46:58 +0300

    v4.4.15

--- Localization/ProjectDescriptions.cs
+++ Localization/ProjectDescriptions.cs
@@ -43,6 +43,6 @@ namespace StockSharp.Localization
 		/// <summary>
 		/// Gets version information.
 		/// </summary>
-		public const string Version = "4.4.14";
+		public const string Version = "4.4.15";
 	}
 }
