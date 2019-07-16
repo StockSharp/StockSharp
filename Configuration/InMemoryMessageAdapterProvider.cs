c168555925cedd2ce95244841f7f0bdea1c3426b
StockSharp Dev1 <mika@stocksharp.com>
2019-07-16 11:51:38 +0300

    InMemoryMessageAdapterProvider refactoring.

--- Configuration/InMemoryMessageAdapterProvider.cs
+++ Configuration/InMemoryMessageAdapterProvider.cs
@@ -7,7 +7,6 @@ namespace StockSharp.Configuration
 
 	using Ecng.Common;
 
-	using StockSharp.Logging;
 	using StockSharp.Messages;
 
 	/// <summary>
@@ -21,30 +20,19 @@ namespace StockSharp.Configuration
 		/// <param name="currentAdapters">All currently available adapters.</param>
 		public InMemoryMessageAdapterProvider(IEnumerable<IMessageAdapter> currentAdapters)
 		{
-			var idGenerator = new IncrementalIdGenerator();
-
-			PossibleAdapters = Extensions.Adapters.Select(type => type.CreateAdapter(idGenerator)).ToArray();
 			CurrentAdapters = currentAdapters ?? throw new ArgumentNullException(nameof(currentAdapters));
+
+			var idGenerator = new IncrementalIdGenerator();
+			PossibleAdapters = Extensions.Adapters.Select(type => type.CreateAdapter(idGenerator)).ToArray();
 		}
 
 		/// <inheritdoc />
-		public IEnumerable<IMessageAdapter> CurrentAdapters { get; }
+		public virtual IEnumerable<IMessageAdapter> CurrentAdapters { get; }
 
 		/// <inheritdoc />
-		public IEnumerable<IMessageAdapter> PossibleAdapters { get; }
+		public virtual IEnumerable<IMessageAdapter> PossibleAdapters { get; }
 
 		/// <inheritdoc />
-		public IEnumerable<IMessageAdapter> CreateStockSharpAdapters(IdGenerator transactionIdGenerator, string login, SecureString password)
-		{
-			try
-			{
-				return Extensions.CreateStockSharpAdapters(transactionIdGenerator, login, password);
-			}
-			catch (Exception ex)
-			{
-				ex.LogError();
-				return Enumerable.Empty<IMessageAdapter>();
-			}
-		}
+		public virtual IEnumerable<IMessageAdapter> CreateStockSharpAdapters(IdGenerator transactionIdGenerator, string login, SecureString password) => Enumerable.Empty<IMessageAdapter>();
 	}
 }
