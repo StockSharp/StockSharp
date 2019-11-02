7e5b610a4322d05e22a47933971bd1e3c7eac611
StockSharp Dev1 <mika@stocksharp.com>
2019-11-02 10:02:09 +0300

    IndicatorValue. ValueNN properties added.

--- Algo/Export/IndicatorValue.cs
+++ Algo/Export/IndicatorValue.cs
@@ -25,28 +25,72 @@ namespace StockSharp.Algo.Export
 		/// </summary>
 		public DateTimeOffset Time { get; set; }
 
+		private IIndicatorValue _value;
+
+		/// <summary>
+		/// Value.
+		/// </summary>
+		public IIndicatorValue Value
+		{
+			get => _value;
+			set
+			{
+				_value = value;
+
+				if (value == null)
+					ValuesAsDecimal = null;
+				else
+				{
+					var values = new List<decimal?>(); 
+					FillValues(Value, values);
+					ValuesAsDecimal = values;
+				}
+			}
+		}
+
 		/// <summary>
 		/// Converted to <see cref="decimal"/> type value.
 		/// </summary>
-		public decimal? ValueAsDecimal => ValuesAsDecimal.First();
+		[Obsolete("Use Value1 property.")]
+		public decimal? ValueAsDecimal => Value1;
+
+		/// <summary>
+		/// Converted to <see cref="decimal"/> type value.
+		/// </summary>
+		public decimal? Value1 => ValueAt(0);
+
+		/// <summary>
+		/// Converted to <see cref="decimal"/> type value.
+		/// </summary>
+		public decimal? Value2 => ValueAt(1);
+
+		/// <summary>
+		/// Converted to <see cref="decimal"/> type value.
+		/// </summary>
+		public decimal? Value3 => ValueAt(2);
+
+		/// <summary>
+		/// Converted to <see cref="decimal"/> type value.
+		/// </summary>
+		public decimal? Value4 => ValueAt(3);
+
+		private decimal? ValueAt(int index)
+		{
+			return ValuesAsDecimal?.ElementAtOrDefault(index);
+		}
 
 		/// <summary>
 		/// Converted to <see cref="decimal"/> type values.
 		/// </summary>
-		public IEnumerable<decimal?> ValuesAsDecimal
-		{
-			get
-			{
-				var values = new List<decimal?>(); 
-				FillValues(Value, values);
-				return values;
-			}
-		}
+		public IEnumerable<decimal?> ValuesAsDecimal { get; private set; }
 
 		private static void FillValues(IIndicatorValue value, ICollection<decimal?> values)
 		{
 			if (value == null)
 				throw new ArgumentNullException(nameof(value));
+
+			if (values == null)
+				throw new ArgumentNullException(nameof(values));
 
 			if (value is DecimalIndicatorValue || value is CandleIndicatorValue || value is ShiftedIndicatorValue)
 			{
@@ -67,10 +111,5 @@ namespace StockSharp.Algo.Export
 			else
 				throw new ArgumentOutOfRangeException(nameof(value), value.GetType(), LocalizedStrings.Str1655);
 		}
-
-		/// <summary>
-		/// Value.
-		/// </summary>
-		public IIndicatorValue Value { get; set; }
 	}
 }
