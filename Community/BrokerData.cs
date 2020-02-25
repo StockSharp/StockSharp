7bf312199685f65f7efbb0e54519746d63e7f0e0
StockSharp Dev1 <mika@stocksharp.com>
2020-02-25 22:52:18 +0300

    Community. BrokerData added.

---
+++ Community/BrokerData.cs
@@ -0,0 +1,47 @@
+namespace StockSharp.Community
+{
+	using System.Runtime.Serialization;
+
+	/// <summary>
+	/// Broker information.
+	/// </summary>
+	[DataContract]
+	public class BrokerData
+	{
+		/// <summary>
+		/// Identifier.
+		/// </summary>
+		[DataMember]
+		public long Id { get; set; }
+
+		/// <summary>
+		/// Name.
+		/// </summary>
+		[DataMember]
+		public string Name { get; set; }
+
+		/// <summary>
+		/// Criteria.
+		/// </summary>
+		[DataMember]
+		public long[] Criteria { get; set; }
+
+		/// <summary>
+		/// Open an account link.
+		/// </summary>
+		[DataMember]
+		public string OpenAccountLink { get; set; }
+
+		/// <summary>
+		/// Open a demo account link.
+		/// </summary>
+		[DataMember]
+		public string OpenDemoAccountLink { get; set; }
+
+		/// <summary>
+		/// Picture id.
+		/// </summary>
+		[DataMember]
+		public long? Picture { get; set; }
+	}
+}
