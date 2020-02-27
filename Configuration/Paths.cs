e3de4acf03635646823f1ea4fac703b29fb80613
StockSharp Dev1 <mika@stocksharp.com>
2020-02-27 12:49:24 +0300

    CommunityUrls joined with Paths.

--- Configuration/Paths.cs
+++ Configuration/Paths.cs
@@ -6,6 +6,8 @@
 	using Ecng.Common;
 	using Ecng.Configuration;
 
+	using StockSharp.Localization;
+
 	/// <summary>
 	/// System paths.
 	/// </summary>
@@ -79,5 +81,81 @@
 		/// The path to the directory with snapshots of market data.
 		/// </summary>
 		public static readonly string SnapshotsDir;
+
+		/// <summary>
+		/// Get website url.
+		/// </summary>
+		/// <returns>Localized url.</returns>
+		public static string GetWebSiteUrl()
+		{
+			return $"https://stocksharp.{LocalizedStrings.Domain}";
+		}
+
+		/// <summary>
+		/// Get user url.
+		/// </summary>
+		/// <param name="userId">User id.</param>
+		/// <returns>Localized url.</returns>
+		public static string GetUserUrl(long userId)
+		{
+			return $"{GetWebSiteUrl()}/users/{userId}/";
+		}
+
+		/// <summary>
+		/// Get strategy url.
+		/// </summary>
+		/// <param name="robotId">The strategy identifier.</param>
+		/// <returns>Localized url.</returns>
+		public static string GetRobotLink(long robotId)
+		{
+			return $"{GetWebSiteUrl()}/robot/{robotId}/";
+		}
+
+		/// <summary>
+		/// Get file url.
+		/// </summary>
+		/// <param name="fileId">File ID.</param>
+		/// <returns>Localized url.</returns>
+		public static string GetFileLink(object fileId)
+		{
+			return $"{GetWebSiteUrl()}/file/{fileId}/";
+		}
+
+		/// <summary>
+		/// To create localized url.
+		/// </summary>
+		/// <param name="docUrl">Help topic.</param>
+		/// <returns>Localized url.</returns>
+		public static string GetDocUrl(string docUrl)
+		{
+			return $"https://doc.stocksharp.{LocalizedStrings.Domain}/html/{docUrl}";
+		}
+
+		/// <summary>
+		/// Get open account url.
+		/// </summary>
+		/// <returns>Localized url.</returns>
+		public static string GetOpenAccountUrl()
+		{
+			return $"{GetWebSiteUrl()}/broker/openaccount/";
+		}
+
+		/// <summary>
+		/// Get sign up url.
+		/// </summary>
+		/// <returns>Localized url.</returns>
+		public static string GetSignUpUrl()
+		{
+			return $"{GetWebSiteUrl()}/register/";
+		}
+
+		/// <summary>
+		/// Get forgot password url.
+		/// </summary>
+		/// <returns>Localized url.</returns>
+		public static string GetForgotUrl()
+		{
+			return $"{GetWebSiteUrl()}/forgot/";
+		}
 	}
 }
