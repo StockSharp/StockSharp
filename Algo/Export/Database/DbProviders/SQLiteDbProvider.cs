5a096f346be6556bcd1fe3712526271bd92e35bc
StockSharp Dev1 <mika@stocksharp.com>
2020-03-04 13:34:45 +0300

    DatabaseConnectionPair moved from Ecng.Xaml.DevExp to Ecng.Data.

--- Algo/Export/Database/DbProviders/SQLiteDbProvider.cs
+++ Algo/Export/Database/DbProviders/SQLiteDbProvider.cs
@@ -21,7 +21,7 @@ namespace StockSharp.Algo.Export.Database.DbProviders
 	using System.Text;
 
 	using Ecng.Common;
-	using Ecng.Xaml.DevExp.Database;
+	using Ecng.Data;
 
 	internal class SQLiteDbProvider : BaseDbProvider
 	{
