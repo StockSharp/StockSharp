1d5cc0a663faa99d665d41152fbaa4060b9cb1fb
StockSharp Dev1 <mika@stocksharp.com>
2020-03-27 10:01:46 +0300

    Config.Adapters. Asm title fix.

--- Configuration.Adapters/Properties/AssemblyInfo.cs
+++ Configuration.Adapters/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("S#.Configuration")]
+[assembly: AssemblyTitle("S#.Configuration (all adapters)")]
 [assembly: AssemblyDescription("S#.Configuration components.")]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
