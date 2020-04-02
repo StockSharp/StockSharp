cc78c9a1bdaddb56f12909dc412c0dad26a93d7f
StockSharp Dev1 <mika@stocksharp.com>
2020-04-02 21:02:07 +0300

    Xml comments.

--- Algo/Storages/StorageCoreSettings.cs
+++ Algo/Storages/StorageCoreSettings.cs
@@ -72,17 +72,36 @@
 		/// </summary>
 		public StorageModes Mode { get; set; } = StorageModes.Incremental;
 
+		/// <summary>
+		/// To get the market-data storage.
+		/// </summary>
+		/// <typeparam name="TMessage">Message type.</typeparam>
+		/// <param name="securityId">Security ID.</param>
+		/// <param name="arg">The parameter associated with the <typeparamref name="TMessage" /> type. For example, <see cref="CandleMessage.Arg"/>.</param>
+		/// <returns>Market-data storage.</returns>
 		public IMarketDataStorage<TMessage> GetStorage<TMessage>(SecurityId securityId, object arg)
 			where TMessage : Message
 		{
 			return (IMarketDataStorage<TMessage>)GetStorage(securityId, typeof(TMessage), arg);
 		}
 
+		/// <summary>
+		/// To get the market-data storage.
+		/// </summary>
+		/// <param name="securityId">Security ID.</param>
+		/// <param name="messageType"></param>
+		/// <param name="arg">The parameter associated with the <paramref name="messageType" /> type. For example, <see cref="CandleMessage.Arg"/>.</param>
+		/// <returns>Market-data storage.</returns>
 		public IMarketDataStorage GetStorage(SecurityId securityId, Type messageType, object arg)
 		{
 			return StorageRegistry.GetStorage(securityId, messageType, arg, Drive, Format);
 		}
 
+		/// <summary>
+		/// Check the specified mode turned on.
+		/// </summary>
+		/// <param name="mode">Storage mode.</param>
+		/// <returns>Check result.</returns>
 		public bool IsMode(StorageModes mode) => mode.Contains(StorageModes.Snapshot);
 
 		void IPersistable.Load(SettingsStorage storage)
