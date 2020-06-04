19ea0ec80a43f527cb9d6f9574522808d3dea07a
StockSharp Dev1 <mika@stocksharp.com>
2020-06-04 20:09:19 +0700

    HistorySourceMessage, GeneratorMessage implemented Clone.

--- Algo/Testing/HistorySourceMessage.cs
+++ Algo/Testing/HistorySourceMessage.cs
@@ -37,5 +37,27 @@ namespace StockSharp.Algo.Testing
 		/// Callback to retrieve historical data for the specified date.
 		/// </summary>
 		public Func<DateTimeOffset, IEnumerable<Message>> GetMessages { get; set; }
+
+		/// <summary>
+		/// Copy the message into the <paramref name="destination" />.
+		/// </summary>
+		/// <param name="destination">The object, to which copied information.</param>
+		public void CopyTo(HistorySourceMessage destination)
+		{
+			base.CopyTo(destination);
+
+			destination.GetMessages = GetMessages;
+		}
+
+		/// <summary>
+		/// Create a copy of <see cref="HistorySourceMessage"/>.
+		/// </summary>
+		/// <returns>Copy.</returns>
+		public override Message Clone()
+		{
+			var clone = new HistorySourceMessage();
+			CopyTo(clone);
+			return clone;
+		}
 	}
 }
