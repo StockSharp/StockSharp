96bf4c471aac82213b74d07bf2d1538bb6dbe4bc
StockSharp Dev1 <mika@stocksharp.com>
2020-06-30 20:15:22 +0700

    TransactionBinarySnapshotSerializer. New fields supported.

--- Algo/Storages/Binary/Snapshot/SnapshotVersions.cs
+++ Algo/Storages/Binary/Snapshot/SnapshotVersions.cs
@@ -6,5 +6,6 @@ namespace StockSharp.Algo.Storages.Binary.Snapshot
 	{
 		public static readonly Version V20 = new Version(2, 0);
 		public static readonly Version V21 = new Version(2, 1);
+		public static readonly Version V22 = new Version(2, 2);
 	}
 }
