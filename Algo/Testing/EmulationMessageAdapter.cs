4673071b65711dc19f08b36b57c233e5769e0580
StockSharp Dev1 <mika@stocksharp.com>
2020-07-06 17:32:08 +0700

    Fix commit 05ace01cf41cbdbd538ff3c6db90bf5e7a1ff948

--- Algo/Testing/EmulationMessageAdapter.cs
+++ Algo/Testing/EmulationMessageAdapter.cs
@@ -106,7 +106,7 @@ namespace StockSharp.Algo.Testing
 		public override IEnumerable<MessageTypes> SupportedOutMessages => InnerAdapter.SupportedOutMessages.Concat(Emulator.SupportedOutMessages).Distinct().ToArray();
 
 		/// <inheritdoc />
-		public override bool? IsPositionsEmulationRequired => false;
+		public override bool? IsPositionsEmulationRequired => Emulator.IsPositionsEmulationRequired;
 
 		private void SendToEmulator(Message message)
 		{
