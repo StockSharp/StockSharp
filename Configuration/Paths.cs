bc4ebde1119c4b37e37b008a1b61d31d7979d97b
StockSharp Dev1 <mika@stocksharp.com>
2020-10-09 15:01:32 +0300

    Code cleanup.

--- Configuration/Paths.cs
+++ Configuration/Paths.cs
@@ -146,77 +146,53 @@
 		/// Get website url.
 		/// </summary>
 		/// <returns>Localized url.</returns>
-		public static string GetWebSiteUrl()
-		{
-			return $"https://stocksharp.{LocalizedStrings.Domain}";
-		}
+		public static string GetWebSiteUrl() => $"https://stocksharp.{LocalizedStrings.Domain}";
 
 		/// <summary>
 		/// Get user url.
 		/// </summary>
 		/// <param name="userId">User id.</param>
 		/// <returns>Localized url.</returns>
-		public static string GetUserUrl(long userId)
-		{
-			return $"{GetWebSiteUrl()}/users/{userId}/";
-		}
+		public static string GetUserUrl(long userId) => $"{GetWebSiteUrl()}/users/{userId}/";
 
 		/// <summary>
 		/// Get strategy url.
 		/// </summary>
 		/// <param name="robotId">The strategy identifier.</param>
 		/// <returns>Localized url.</returns>
-		public static string GetRobotLink(long robotId)
-		{
-			return $"{GetWebSiteUrl()}/robot/{robotId}/";
-		}
+		public static string GetRobotLink(long robotId) => $"{GetWebSiteUrl()}/robot/{robotId}/";
 
 		/// <summary>
 		/// Get file url.
 		/// </summary>
 		/// <param name="fileId">File ID.</param>
 		/// <returns>Localized url.</returns>
-		public static string GetFileLink(object fileId)
-		{
-			return $"{GetWebSiteUrl()}/file/{fileId}/";
-		}
+		public static string GetFileLink(object fileId) => $"{GetWebSiteUrl()}/file/{fileId}/";
 
 		/// <summary>
 		/// To create localized url.
 		/// </summary>
 		/// <param name="docUrl">Help topic.</param>
 		/// <returns>Localized url.</returns>
-		public static string GetDocUrl(string docUrl)
-		{
-			return $"https://doc.stocksharp.{LocalizedStrings.Domain}/html/{docUrl}";
-		}
+		public static string GetDocUrl(string docUrl) => $"https://doc.stocksharp.{LocalizedStrings.Domain}/html/{docUrl}";
 
 		/// <summary>
 		/// Get open account url.
 		/// </summary>
 		/// <returns>Localized url.</returns>
-		public static string GetOpenAccountUrl()
-		{
-			return $"{GetWebSiteUrl()}/broker/openaccount/";
-		}
+		public static string GetOpenAccountUrl() => $"{GetWebSiteUrl()}/broker/openaccount/";
 
 		/// <summary>
 		/// Get sign up url.
 		/// </summary>
 		/// <returns>Localized url.</returns>
-		public static string GetSignUpUrl()
-		{
-			return $"{GetWebSiteUrl()}/register/";
-		}
+		public static string GetSignUpUrl() => $"{GetWebSiteUrl()}/register/";
 
 		/// <summary>
 		/// Get forgot password url.
 		/// </summary>
 		/// <returns>Localized url.</returns>
-		public static string GetForgotUrl()
-		{
-			return $"{GetWebSiteUrl()}/forgot/";
-		}
+		public static string GetForgotUrl() => $"{GetWebSiteUrl()}/forgot/";
 
 		/// <summary>
 		/// Get currently installed version of the product.
