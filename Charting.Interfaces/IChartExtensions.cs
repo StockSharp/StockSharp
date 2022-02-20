ab391daa1163f694fa6397f2dbebba4b6aed50c2
mikasoukhov <mika@stocksharp.com>
2022-02-20 15:58:21 +0300

    IIndicatorProvider

---
+++ Charting.Interfaces/IChartExtensions.cs
@@ -0,0 +1,63 @@
+﻿namespace StockSharp.Charting
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+
+	using Ecng.Collections;
+	using Ecng.Common;
+	using Ecng.Configuration;
+
+	using StockSharp.Algo.Indicators;
+
+	/// <summary>
+	/// <see cref="IChart"/> extensions.
+	/// </summary>
+	public static class IChartExtensions
+	{
+		/// <summary>
+		/// <see cref="IIndicatorProvider"/>
+		/// </summary>
+		public static IIndicatorProvider IndicatorProvider => ConfigManager.GetService<IIndicatorProvider>();
+
+		/// <summary>
+		/// Exclude obsolete indicators.
+		/// </summary>
+		/// <param name="types">All indicator types.</param>
+		/// <returns>Filtered collection.</returns>
+		public static IEnumerable<IndicatorType> ExcludeObsolete(this IEnumerable<IndicatorType> types)
+		{
+			return types.Where(t => !t.Indicator.IsObsolete());
+		}
+
+		/// <summary>
+		/// Fill <see cref="IChart.IndicatorTypes"/> using <see cref="IIndicatorProvider"/>.
+		/// </summary>
+		/// <param name="chart">Chart.</param>
+		public static void FillIndicators(this IChart chart)
+		{
+			if (chart == null)
+				throw new ArgumentNullException(nameof(chart));
+
+			chart.IndicatorTypes.Clear();
+			chart.IndicatorTypes.AddRange(IndicatorProvider.GetIndicatorTypes().Where(it => it.Indicator.IsIndicatorSupportedByChart()).ExcludeObsolete());
+		}
+
+		private static readonly Type[] _chartUnsupportedIndicators =
+		{
+			typeof(Level1Indicator),
+			typeof(Covariance),
+			typeof(Correlation),
+		};
+
+		/// <summary>
+		/// Check if indicator is supported by chart.
+		/// </summary>
+		public static bool IsIndicatorSupportedByChart(this Type itype)
+		{
+			return
+				typeof(IIndicator).IsAssignableFrom(itype) &&
+				!_chartUnsupportedIndicators.Any(t => t.IsAssignableFrom(itype));
+		}
+	}
+}
---
+++ Charting.Interfaces/IIndicatorProvider.cs
@@ -0,0 +1,18 @@
+﻿namespace StockSharp.Charting
+{
+	using System.Collections.Generic;
+
+	using StockSharp.Algo.Indicators;
+
+	/// <summary>
+	/// Provider <see cref="IndicatorType"/>.
+	/// </summary>
+	public interface IIndicatorProvider
+	{
+		/// <summary>
+		/// Get all indicator types.
+		/// </summary>
+		/// <returns>All indicator types.</returns>
+		IEnumerable<IndicatorType> GetIndicatorTypes();
+	}
+}
