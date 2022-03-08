c5b76961fb00aca47c1a1a6d912c3ec5eb233c4d
mikasoukhov <mika@stocksharp.com>
2022-03-08 19:06:32 +0300

    ProductIdAttribute

---
+++ Configuration/ProductIdAttribute.cs
@@ -0,0 +1,25 @@
+﻿namespace StockSharp.Configuration
+{
+	using System;
+
+	/// <summary>
+	/// Product id attribute.
+	/// </summary>
+	[AttributeUsage(AttributeTargets.Assembly, Inherited = false, AllowMultiple = false)]
+	public class ProductIdAttribute : Attribute
+	{
+		/// <summary>
+		/// Product id.
+		/// </summary>
+		public long ProductId { get; }
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="ProductIdAttribute"/>.
+		/// </summary>
+		/// <param name="productId"><see cref="ProductId"/></param>
+		public ProductIdAttribute(long productId)
+		{
+			ProductId = productId;
+		}
+	}
+}
