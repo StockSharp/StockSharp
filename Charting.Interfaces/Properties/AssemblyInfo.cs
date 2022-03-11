2d426a00e3d80123342444e87b61a32ae7f5744b
Sergey Sokolov <sokolovsa@gmail.com>
2022-03-11 13:38:52 +0300

    added asm info

---
+++ Charting.Interfaces/Properties/AssemblyInfo.cs
@@ -0,0 +1,26 @@
+﻿#region S# License
+/******************************************************************************************
+NOTICE!!!  This program and source code is owned and licensed by
+StockSharp, LLC, www.stocksharp.com
+Viewing or use of this code requires your acceptance of the license
+agreement found at https://github.com/StockSharp/StockSharp/blob/master/LICENSE
+Removal of this comment is a violation of the license agreement.
+
+Project: %Namespace%.Charting.Interfaces
+File: AssemblyInfo.cs
+Created: 2015, 11, 11, 2:32 PM
+
+Copyright 2010 by StockSharp, LLC
+*******************************************************************************************/
+#endregion S# License
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("S#.Charting.Interfaces")]
+[assembly: AssemblyDescription("S#.Charting.Interfaces")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("44953EAC-4E10-4680-BAF3-B4C67F638331")]
