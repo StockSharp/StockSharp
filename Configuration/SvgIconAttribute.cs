f022b40d4f025519cf9e5438e1b6778b8ddd9890
mikasoukhov <mika@stocksharp.com>
2022-08-13 12:28:15 +0300

    SvgIconAttribute

---
+++ Configuration/SvgIconAttribute.cs
@@ -0,0 +1,19 @@
+﻿namespace StockSharp.Configuration
+{
+	using Ecng.ComponentModel;
+
+	/// <summary>
+	/// Specify icon, located in S#.Xaml library.
+	/// </summary>
+	public class SvgIconAttribute : IconAttribute
+	{
+		/// <summary>
+		/// Create <see cref="SvgIconAttribute"/>.
+		/// </summary>
+		/// <param name="icon">Icon url.</param>
+		public SvgIconAttribute(string icon)
+			: base($"/StockSharp.Xaml;component/IconsSvg/{icon}", true)
+		{
+		}
+	}
+}
