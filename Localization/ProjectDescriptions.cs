9f91f1fa192a075ee12011171d7e3aa8c66ae089
mikasoukhov <mika@stocksharp.com>
2022-09-13 13:19:56 +0300

    Fix commit 119801f428a79c0e5fdbeee72a4d2134cc4c7682

---
+++ Localization/ProjectDescriptions.cs
@@ -0,0 +1,49 @@
+﻿namespace StockSharp.Localization
+{
+	using System;
+	using System.Reflection;
+
+	using Ecng.Common;
+
+	/// <summary>
+	/// Information for an assembly manifest.
+	/// </summary>
+	public static class ProjectDescriptions
+	{
+		static ProjectDescriptions()
+		{
+			var asm = typeof(ProjectDescriptions).Assembly;
+
+			Company = asm.GetAttribute<AssemblyCompanyAttribute>()?.Company;
+			Product = asm.GetAttribute<AssemblyProductAttribute>()?.Product;
+			Copyright = asm.GetAttribute<AssemblyCopyrightAttribute>()?.Copyright;
+			Trademark = asm.GetAttribute<AssemblyTrademarkAttribute>()?.Trademark;
+			Version = asm.GetName().Version;
+		}
+
+		/// <summary>
+		/// Gets company information.
+		/// </summary>
+		public static readonly string Company;
+
+		/// <summary>
+		/// Gets product information.
+		/// </summary>
+		public static readonly string Product;
+
+		/// <summary>
+		/// Gets copyright information.
+		/// </summary>
+		public static readonly string Copyright;
+
+		/// <summary>
+		/// Gets trademark information.
+		/// </summary>
+		public static readonly string Trademark;
+
+		/// <summary>
+		/// Gets version information.
+		/// </summary>
+		public static readonly Version Version;
+	}
+}
