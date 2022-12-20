4adbc7c03e337c0fd22756b454ce0c7f097d9e03
mikasoukhov <mika@stocksharp.com>
2022-12-20 23:39:58 +0300

    IConnector. ConnectAsync and DisconnectAsync extensions.

---
+++ BusinessEntities/IConnectorAsyncExtensions.cs
@@ -0,0 +1,90 @@
+﻿namespace StockSharp.Algo;
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Ecng.Common;
+
+using StockSharp.BusinessEntities;
+using StockSharp.Messages;
+
+/// <summary>
+/// Async extensions for <see cref="IConnector"/>.
+/// </summary>
+public static class IConnectorAsyncExtensions
+{
+	/// <summary>
+	/// Async version <see cref="IConnector.Connect"/>.
+	/// </summary>
+	/// <param name="connector"><see cref="IConnector"/>.</param>
+	/// <param name="cancellationToken"><see cref="CancellationToken"/>.</param>
+	/// <returns><see cref="ValueTask"/>.</returns>
+	public static async ValueTask ConnectAsync(this IConnector connector, CancellationToken cancellationToken)
+	{
+		if (connector is null)
+			throw new ArgumentNullException(nameof(connector));
+
+		if (connector.ConnectionState != ConnectionStates.Disconnected)
+			throw new ArgumentException($"State is {connector.ConnectionState}.", nameof(connector));
+
+		var tcs = AsyncHelper.CreateTaskCompletionSource<ConnectionStates>();
+
+		using var _ = cancellationToken.Register(tcs.SetCanceled);
+
+		void OnConnected() => tcs.SetResult(ConnectionStates.Connected);
+		void OnConnectionError(Exception ex) => tcs.SetException(ex);
+
+		connector.Connected += OnConnected;
+		connector.ConnectionError += OnConnectionError;
+
+		try
+		{
+			connector.Connect();
+
+			await tcs.Task;
+		}
+		finally
+		{
+			connector.Connected -= OnConnected;
+			connector.ConnectionError -= OnConnectionError;
+		}
+	}
+
+	/// <summary>
+	/// Async version <see cref="IConnector.Disconnect"/>.
+	/// </summary>
+	/// <param name="connector"><see cref="IConnector"/>.</param>
+	/// <param name="cancellationToken"><see cref="CancellationToken"/>.</param>
+	/// <returns><see cref="ValueTask"/>.</returns>
+	public static async ValueTask DisconnectAsync(this IConnector connector, CancellationToken cancellationToken)
+	{
+		if (connector is null)
+			throw new ArgumentNullException(nameof(connector));
+
+		if (connector.ConnectionState != ConnectionStates.Connected)
+			throw new ArgumentException($"State is {connector.ConnectionState}.", nameof(connector));
+
+		var tcs = AsyncHelper.CreateTaskCompletionSource<ConnectionStates>();
+
+		using var _ = cancellationToken.Register(tcs.SetCanceled);
+
+		void OnDisconnected() => tcs.SetResult(ConnectionStates.Disconnected);
+		void OnConnectionError(Exception ex) => tcs.SetException(ex);
+
+		connector.Disconnected += OnDisconnected;
+		connector.ConnectionError += OnConnectionError;
+
+		try
+		{
+			connector.Disconnect();
+
+			await tcs.Task;
+		}
+		finally
+		{
+			connector.Disconnected -= OnDisconnected;
+			connector.ConnectionError -= OnConnectionError;
+		}
+	}
+}
