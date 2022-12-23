e2d3fa9bf043878a6b6dca967c7fceeffc77aa7c
mikasoukhov <mika@stocksharp.com>
2022-12-24 01:43:33 +0300

    EmptyAnalyticsStrategy

---
+++ Algo/Strategies/Analytics/EmptyAnalyticsStrategy.cs
@@ -0,0 +1,36 @@
+﻿namespace StockSharp.Studio.Strategies
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+
+	using Ecng.Common;
+	using Ecng.ComponentModel;
+	using Ecng.Collections;
+
+	using StockSharp.Algo.Storages;
+	using StockSharp.Algo.Strategies;
+	using StockSharp.Algo.Strategies.Analytics;
+
+	/// <summary>
+	/// The empty analytic strategy.
+	/// </summary>
+	public class EmptyAnalyticsStrategy : BaseAnalyticsStrategy
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="EmptyAnalyticsStrategy"/>.
+		/// </summary>
+		public EmptyAnalyticsStrategy()
+		{
+		}
+
+		/// <summary>
+		/// To analyze.
+		/// </summary>
+		protected override void OnAnalyze()
+		{
+			// notify the script stopped
+			Stop();
+		}
+	}
+}
