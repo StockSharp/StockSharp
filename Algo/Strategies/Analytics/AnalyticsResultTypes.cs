fd3b8698243907cd12f41a89e4d5e76882aa489e
mikasoukhov <mika@stocksharp.com>
2022-12-24 01:43:18 +0300

    Code cleanup.

---
+++ Algo/Strategies/Analytics/AnalyticsResultTypes.cs
@@ -0,0 +1,35 @@
+﻿namespace StockSharp.Algo.Strategies.Analytics;
+
+using System.ComponentModel.DataAnnotations;
+
+using StockSharp.Localization;
+
+/// <summary>
+/// Types of result.
+/// </summary>
+public enum AnalyticsResultTypes
+{
+	/// <summary>
+	/// Table.
+	/// </summary>
+	[Display(ResourceType = typeof(LocalizedStrings), Name = LocalizedStrings.Str3280Key)]
+	Grid,
+
+	/// <summary>
+	/// Bubble chart.
+	/// </summary>
+	[Display(ResourceType = typeof(LocalizedStrings), Name = LocalizedStrings.Str1977Key)]
+	Bubble,
+
+	/// <summary>
+	/// Histogram.
+	/// </summary>
+	[Display(ResourceType = typeof(LocalizedStrings), Name = LocalizedStrings.Str1976Key)]
+	Histogram,
+
+	/// <summary>
+	/// Heatmap.
+	/// </summary>
+	[Display(ResourceType = typeof(LocalizedStrings), Name = LocalizedStrings.HeatmapKey)]
+	Heatmap,
+}
--- Algo/Strategies/Analytics/BaseAnalyticsStrategy.cs
+++ Algo/Strategies/Analytics/BaseAnalyticsStrategy.cs
@@ -24,36 +24,6 @@ namespace StockSharp.Algo.Strategies.Analytics
 	using StockSharp.Localization;
 
 	/// <summary>
-	/// Types of result.
-	/// </summary>
-	public enum AnalyticsResultTypes
-	{
-		/// <summary>
-		/// Table.
-		/// </summary>
-		[Display(ResourceType = typeof(LocalizedStrings), Name = LocalizedStrings.Str3280Key)]
-		Grid,
-
-		/// <summary>
-		/// Bubble chart.
-		/// </summary>
-		[Display(ResourceType = typeof(LocalizedStrings), Name = LocalizedStrings.Str1977Key)]
-		Bubble,
-
-		/// <summary>
-		/// Histogram.
-		/// </summary>
-		[Display(ResourceType = typeof(LocalizedStrings), Name = LocalizedStrings.Str1976Key)]
-		Histogram,
-
-		/// <summary>
-		/// Heatmap.
-		/// </summary>
-		[Display(ResourceType = typeof(LocalizedStrings), Name = LocalizedStrings.HeatmapKey)]
-		Heatmap,
-	}
-
-	/// <summary>
 	/// The base analytic strategy.
 	/// </summary>
 	public abstract class BaseAnalyticsStrategy : Strategy
