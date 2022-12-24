533384e80949c2ca39d86193ada8a6580bd543d5
mikasoukhov <mika@stocksharp.com>
2022-12-24 13:36:02 +0300

    Algo.Compilation moved from Xaml.

---
+++ Algo/Compilation/CodeExtensions.cs
@@ -0,0 +1,69 @@
+namespace StockSharp.Algo.Compilation
+{
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Threading;
+
+	using Ecng.Compilation;
+
+	/// <summary>
+	/// Extension class.
+	/// </summary>
+	public static class CodeExtensions
+	{
+		/// <summary>
+		/// To compile the code.
+		/// </summary>
+		/// <param name="compiler">Compiler.</param>
+		/// <param name="code">Code.</param>
+		/// <param name="name">The reference name.</param>
+		/// <param name="references">References.</param>
+		/// <param name="cancellationToken"><see cref="CancellationToken"/>.</param>
+		/// <returns>The result of the compilation.</returns>
+		public static CompilationResult CompileCode(this ICompiler compiler, string code, string name, IEnumerable<CodeReference> references, CancellationToken cancellationToken = default)
+			=> compiler.Compile(new(), name, code, references.Where(r => r.IsValid).Select(r => r.FullLocation).ToArray(), cancellationToken);
+
+		private static readonly IEnumerable<string> _defaultReferences = new[]
+		{
+			"mscorlib",
+			"netstandard",
+
+			"System",
+			"System.Core",
+			"System.Configuration",
+			"System.Xml",
+			"System.Runtime",
+			"System.Linq",
+			"System.Private.CoreLib",
+			"System.ObjectModel",
+			"System.ComponentModel.Primitives",
+			"System.ComponentModel.TypeConverter",
+			"System.ComponentModel.Annotations",
+
+			"Ecng.Common",
+			"Ecng.Collections",
+			"Ecng.ComponentModel",
+			"Ecng.Configuration",
+			"Ecng.Localization",
+			"Ecng.Serialization",
+
+			"StockSharp.Algo",
+			"StockSharp.Algo.Strategies",
+			"StockSharp.Messages",
+			"StockSharp.BusinessEntities",
+			"StockSharp.Logging",
+			"StockSharp.Localization",
+			"StockSharp.Diagram.Core",
+			"StockSharp.Charting.Interfaces",
+			"StockSharp.Alerts.Interfaces",
+		};
+
+		/// <summary>
+		/// Default references.
+		/// </summary>
+		public static IEnumerable<CodeReference> DefaultReferences
+			=>	_defaultReferences
+				.Select(r => new CodeReference { Location = $"{r}.dll" })
+				.ToArray();
+	}
+}
---
+++ Algo/Compilation/CodeReference.cs
@@ -0,0 +1,85 @@
+namespace StockSharp.Algo.Compilation
+{
+	using System.IO;
+
+	using Ecng.Common;
+	using Ecng.Compilation;
+	using Ecng.Serialization;
+
+	/// <summary>
+	/// The reference to the .NET assembly.
+	/// </summary>
+	public class CodeReference : IPersistable
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="CodeReference"/>.
+		/// </summary>
+		public CodeReference()
+		{
+		}
+
+		/// <summary>
+		/// The assembly name.
+		/// </summary>
+		public string Name => Path.GetFileNameWithoutExtension(Location);
+
+		/// <summary>
+		/// The path to the assembly.
+		/// </summary>
+		public string Location { get; set; }
+
+		/// <summary>
+		/// <see cref="Location"/>.
+		/// </summary>
+		public string FullLocation
+		{
+			get
+			{
+				var location = Location;
+
+				if (File.Exists(location))
+					return location;
+
+				var fileName = Path.GetFileName(Location);
+
+				if (location.EqualsIgnoreCase(fileName))
+				{
+					var tmp = Path.Combine(Directory.GetCurrentDirectory(), fileName);
+
+					if (File.Exists(tmp))
+						return tmp;
+
+					tmp = Path.Combine(ICompilerExtensions.RuntimePath, fileName);
+
+					if (File.Exists(tmp))
+						return tmp;
+				}
+
+				return location;
+			}
+		}
+
+		/// <summary>
+		/// Is valid.
+		/// </summary>
+		public bool IsValid => File.Exists(FullLocation);
+
+		/// <summary>
+		/// Load settings.
+		/// </summary>
+		/// <param name="storage">Settings storage.</param>
+		public void Load(SettingsStorage storage)
+		{
+			Location = storage.GetValue<string>(nameof(Location));
+		}
+
+		/// <summary>
+		/// Save settings.
+		/// </summary>
+		/// <param name="storage">Settings storage.</param>
+		public void Save(SettingsStorage storage)
+		{
+			storage.SetValue(nameof(Location), Location);
+		}
+	}
+}
