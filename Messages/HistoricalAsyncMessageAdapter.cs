df6c5b8606d012b9a16058c6d669800ea708d273
mikasoukhov <mika@stocksharp.com>
2023-02-12 23:38:54 +0300

    HistoricalAsyncMessageAdapter auto fill null dates.

--- Messages/HistoricalAsyncMessageAdapter.cs
+++ Messages/HistoricalAsyncMessageAdapter.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 
 using Ecng.Common;
@@ -38,5 +39,46 @@ public abstract class HistoricalAsyncMessageAdapter : AsyncMessageAdapter
 	{
 		SendOutMessage(new ResetMessage());
 		return default;
+	}
+
+	/// <inheritdoc />
+	protected override ValueTask OnRunSubscriptionAsync(MarketDataMessage mdMsg, CancellationToken cancellationToken)
+	{
+		if (mdMsg.IsSubscribe)
+		{
+			var now = DateTimeOffset.UtcNow;
+
+			var from = mdMsg.From;
+			var to = mdMsg.To;
+
+			if (from > now)
+			{
+				SendSubscriptionFinished(mdMsg.TransactionId);
+				return default;
+			}
+
+			if (to is null || from is null)
+			{
+				mdMsg = (MarketDataMessage)mdMsg.Clone();
+
+				mdMsg.To ??= now;
+
+				if (from is null)
+				{
+					var isTick =
+						mdMsg.DataType2 == DataType.Level1 ||
+						mdMsg.DataType2 == DataType.Ticks ||
+						mdMsg.DataType2 == DataType.OrderLog ||
+						mdMsg.DataType2 == DataType.MarketDepth;
+
+					var isSmallTf = !isTick && mdMsg.DataType2.IsTFCandles && mdMsg.DataType2.GetTimeFrame().TotalMinutes < 1;
+					var isBigTf = !isTick && mdMsg.DataType2.IsTFCandles && mdMsg.DataType2.GetTimeFrame().TotalDays > 1;
+
+					mdMsg.From = mdMsg.To.Value.AddDays(isTick || isSmallTf ? -1 : (isBigTf ? -30 : -7));
+				}
+			}
+		}
+
+		return base.OnRunSubscriptionAsync(mdMsg, cancellationToken);
 	}
 }
