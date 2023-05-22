092f872705576c680d8421c158a38ae931ff3936
mikasoukhov <mika@stocksharp.com>
2023-05-22 20:30:08 +0300

    BatchEmulation reverted back for backward compatibility.

---
+++ Algo/Strategies/Testing/BatchEmulation.cs
@@ -0,0 +1,60 @@
+﻿namespace StockSharp.Algo.Strategies.Testing;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using StockSharp.Algo.Storages;
+using StockSharp.BusinessEntities;
+
+/// <summary>
+/// </summary>
+[Obsolete("Use BruteForceOptimizer.")]
+public class BatchEmulation : BruteForceOptimizer
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="BatchEmulation"/>.
+	/// </summary>
+	/// <param name="securities">Instruments, the operation will be performed with.</param>
+	/// <param name="portfolios">Portfolios, the operation will be performed with.</param>
+	/// <param name="storageRegistry">Market data storage.</param>
+	public BatchEmulation(IEnumerable<Security> securities, IEnumerable<Portfolio> portfolios, IStorageRegistry storageRegistry)
+		: base(securities, portfolios, storageRegistry)
+	{
+	}
+
+	/// <summary>
+	/// Initializes a new instance of the <see cref="BatchEmulation"/>.
+	/// </summary>
+	/// <param name="securityProvider">The provider of information about instruments.</param>
+	/// <param name="portfolioProvider">The portfolio to be used to register orders. If value is not given, the portfolio with default name Simulator will be created.</param>
+	/// <param name="storageRegistry">Market data storage.</param>
+	public BatchEmulation(ISecurityProvider securityProvider, IPortfolioProvider portfolioProvider, IStorageRegistry storageRegistry)
+		: base(securityProvider, portfolioProvider, storageRegistry)
+	{
+	}
+
+	/// <summary>
+	/// Initializes a new instance of the <see cref="BatchEmulation"/>.
+	/// </summary>
+	/// <param name="securityProvider">The provider of information about instruments.</param>
+	/// <param name="portfolioProvider">The portfolio to be used to register orders. If value is not given, the portfolio with default name Simulator will be created.</param>
+	/// <param name="exchangeInfoProvider">Exchanges and trading boards provider.</param>
+	/// <param name="storageRegistry">Market data storage.</param>
+	/// <param name="storageFormat">The format of market data. <see cref="StorageFormats.Binary"/> is used by default.</param>
+	/// <param name="drive">The storage which is used by default. By default, <see cref="IStorageRegistry.DefaultDrive"/> is used.</param>
+	public BatchEmulation(ISecurityProvider securityProvider, IPortfolioProvider portfolioProvider, IExchangeInfoProvider exchangeInfoProvider, IStorageRegistry storageRegistry, StorageFormats storageFormat = StorageFormats.Binary, IMarketDataDrive drive = null)
+		: base(securityProvider, portfolioProvider, exchangeInfoProvider, storageRegistry, storageFormat, drive)
+	{
+	}
+
+	/// <summary>
+	/// Start emulation.
+	/// </summary>
+	/// <param name="strategies">The strategies.</param>
+	/// <param name="iterationCount">Iteration count.</param>
+	public void Start(IEnumerable<Strategy> strategies, int iterationCount)
+	{
+		Start(strategies.Select(s => (s, s.Parameters.Values.ToArray())), iterationCount);
+	}
+}
