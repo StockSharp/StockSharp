c535d3215a9e21ae875139bfbb688f37d3202b27
mikasoukhov <mika@stocksharp.com>
2023-06-14 11:43:05 +0300

    IStrategyIdMessage. StrategyId is readonly.

--- Messages/IStrategyIdMessage.cs
+++ Messages/IStrategyIdMessage.cs
@@ -3,11 +3,11 @@
 	/// <summary>
 	/// The interface describing an message with <see cref="StrategyId"/> property.
 	/// </summary>
-	public interface IStrategyIdMessage : IMessage
+	public interface IStrategyIdMessage
 	{
 		/// <summary>
 		/// Strategy id.
 		/// </summary>
-		string StrategyId { get; set; }
+		string StrategyId { get; }
 	}
 }
