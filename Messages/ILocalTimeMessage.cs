3f7604e234e44a8def36ca696d342cae49b816aa
mikasoukhov <mika@stocksharp.com>
2023-06-14 11:36:27 +0300

    ILocalTimeMessage

---
+++ Messages/ILocalTimeMessage.cs
@@ -0,0 +1,14 @@
+﻿namespace StockSharp.Messages;
+
+using System;
+
+/// <summary>
+/// The interface describing an message with <see cref="LocalTime"/> property.
+/// </summary>
+public interface ILocalTimeMessage
+{
+	/// <summary>
+	/// Local timestamp when a message was received/created.
+	/// </summary>
+	DateTimeOffset LocalTime { get; set; }
+}
--- Messages/IMessage.cs
+++ Messages/IMessage.cs
@@ -5,17 +5,12 @@ namespace StockSharp.Messages
 	/// <summary>
 	/// The interface describing an message with <see cref="Type"/> method.
 	/// </summary>
-	public interface IMessage : ICloneable
+	public interface IMessage : ILocalTimeMessage, ICloneable
 	{
 		/// <summary>
 		/// Message type.
 		/// </summary>
 		MessageTypes Type { get; }
-
-		/// <summary>
-		/// Local timestamp when a message was received/created.
-		/// </summary>
-		DateTimeOffset LocalTime { get; set; }
 
 		/// <summary>
 		/// Source adapter. Can be <see langword="null" />.
