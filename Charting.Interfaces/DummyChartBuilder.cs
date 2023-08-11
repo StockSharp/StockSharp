701c27caf78c189d7892b51814c8dd311b344050
mikasoukhov <mika@stocksharp.com>
2023-08-11 14:48:29 +0300

    Fix commit 402a0d66f42023b8c3dd9a280543703ca091f062

--- Charting.Interfaces/DummyChartBuilder.cs
+++ Charting.Interfaces/DummyChartBuilder.cs
@@ -5,8 +5,10 @@ using System.ComponentModel;
 using System.Drawing;
 
 using Ecng.Collections;
+using Ecng.Common;
 using Ecng.ComponentModel;
 using Ecng.Serialization;
+using Ecng.Drawing;
 
 /// <summary>
 /// Dummy implementation of <see cref="IChartBuilder"/>.
@@ -27,158 +29,284 @@ public class DummyChartBuilder : IChartBuilder
 			remove { }
 		}
 
-		void IPersistable.Load(SettingsStorage storage) { }
+		public virtual void Load(SettingsStorage storage) { }
 		void IPersistable.Save(SettingsStorage storage) { }
 	}
 
 	private class DummyPart<T> : DummyPersistable, IChartPart<T>
 		where T : IChartPart<T>
 	{
-		Guid IChartPart<T>.Id { get; } = Guid.NewGuid();
+		public Guid Id { get; private set; } = Guid.NewGuid();
+
+		public override void Load(SettingsStorage storage)
+		{
+			base.Load(storage);
+
+			Id = storage.GetValue<Guid>(nameof(Id));
+		}
 	}
 
 	private class DummyElement : DummyPart<IChartElement>, IChartElement
 	{
-		string IChartElement.FullTitle { get; set; }
-		bool IChartElement.IsVisible { get; set; }
-		bool IChartElement.IsLegend { get; set; }
-		string IChartElement.XAxisId { get; set; }
-		string IChartElement.YAxisId { get; set; }
+		public string FullTitle { get; set; }
+		public bool IsVisible { get; set; }
+		public bool IsLegend { get; set; }
+		public string XAxisId { get; set; }
+		public string YAxisId { get; set; }
 		Func<IComparable, Color?> IChartElement.Colorer { get; set; }
 
-		IChartAxis IChartElement.XAxis => throw new NotImplementedException();
-		IChartAxis IChartElement.YAxis => throw new NotImplementedException();
+		IChartAxis IChartElement.XAxis => throw new NotSupportedException();
+		IChartAxis IChartElement.YAxis => throw new NotSupportedException();
 
-		IChart IChartElement.Chart => throw new NotImplementedException();
-		IChartArea IChartElement.ChartArea => throw new NotImplementedException();
-		IChartArea IChartElement.PersistantChartArea => throw new NotImplementedException();
+		IChart IChartElement.Chart => throw new NotSupportedException();
+		IChartArea IChartElement.ChartArea => throw new NotSupportedException();
+		IChartArea IChartElement.PersistantChartArea => throw new NotSupportedException();
+
+		public override void Load(SettingsStorage storage)
+		{
+			base.Load(storage);
+
+			FullTitle = storage.GetValue<string>(nameof(FullTitle));
+			IsVisible = storage.GetValue<bool>(nameof(IsVisible));
+			IsLegend = storage.GetValue<bool>(nameof(IsLegend));
+			XAxisId = storage.GetValue<string>(nameof(XAxisId));
+			YAxisId = storage.GetValue<string>(nameof(YAxisId));
+		}
 	}
 
 	private class DummyActiveOrdersElement : DummyElement, IChartActiveOrdersElement
 	{
-		Color IChartActiveOrdersElement.BuyPendingColor { get; set; }
-		Color IChartActiveOrdersElement.BuyColor { get; set; }
-		Color IChartActiveOrdersElement.BuyBlinkColor { get; set; }
-		Color IChartActiveOrdersElement.SellPendingColor { get; set; }
-		Color IChartActiveOrdersElement.SellColor { get; set; }
-		Color IChartActiveOrdersElement.SellBlinkColor { get; set; }
-		Color IChartActiveOrdersElement.CancelButtonColor { get; set; }
-		Color IChartActiveOrdersElement.CancelButtonBackground { get; set; }
-		Color IChartActiveOrdersElement.ForegroundColor { get; set; }
-		bool IChartActiveOrdersElement.IsAnimationEnabled { get; set; }
+		public Color BuyPendingColor { get; set; }
+		public Color BuyColor { get; set; }
+		public Color BuyBlinkColor { get; set; }
+		public Color SellPendingColor { get; set; }
+		public Color SellColor { get; set; }
+		public Color SellBlinkColor { get; set; }
+		public Color CancelButtonColor { get; set; }
+		public Color CancelButtonBackground { get; set; }
+		public Color ForegroundColor { get; set; }
+		public bool IsAnimationEnabled { get; set; }
+
+		public override void Load(SettingsStorage storage)
+		{
+			base.Load(storage);
+
+			BuyColor = storage.GetValue<int>(nameof(BuyColor)).ToColor();
+			BuyBlinkColor = storage.GetValue<int>(nameof(BuyBlinkColor)).ToColor();
+			BuyPendingColor = storage.GetValue<int>(nameof(BuyPendingColor)).ToColor();
+			SellColor = storage.GetValue<int>(nameof(SellColor)).ToColor();
+			SellBlinkColor = storage.GetValue<int>(nameof(SellBlinkColor)).ToColor();
+			SellPendingColor = storage.GetValue<int>(nameof(SellPendingColor)).ToColor();
+			ForegroundColor = storage.GetValue<int>(nameof(ForegroundColor)).ToColor();
+			CancelButtonColor = storage.GetValue<int>(nameof(CancelButtonColor)).ToColor();
+			CancelButtonBackground = storage.GetValue<int>(nameof(CancelButtonBackground)).ToColor();
+			IsAnimationEnabled = storage.GetValue<bool>(nameof(IsAnimationEnabled));
+		}
 	}
 
 	private class DummyAnnotation : DummyElement, IChartAnnotation
 	{
-		ChartAnnotationTypes IChartAnnotation.Type { get; set; }
+		public ChartAnnotationTypes Type { get; set; }
+
+		public override void Load(SettingsStorage storage)
+		{
+			base.Load(storage);
+
+			Type = storage.GetValue<ChartAnnotationTypes>(nameof(Type));
+		}
 	}
 
 	private class DummyArea : DummyPart<IChartArea>, IChartArea
 	{
-		bool IChartArea.IsAutoRange { get; set; }
-
 		INotifyList<IChartElement> IChartArea.Elements { get; } = new SynchronizedList<IChartElement>();
 		INotifyList<IChartAxis> IChartArea.XAxises { get; } = new SynchronizedList<IChartAxis>();
 		INotifyList<IChartAxis> IChartArea.YAxises { get; } = new SynchronizedList<IChartAxis>();
 
-		ChartAxisType IChartArea.XAxisType { get; set; }
-		string IChartArea.Title { get; set; }
-		double IChartArea.Height { get; set; }
-		IChart IChartArea.Chart => throw new NotImplementedException();
+		public ChartAxisType XAxisType { get; set; } = ChartAxisType.CategoryDateTime;
+		public string Title { get; set; }
+		public double Height { get; set; }
+		IChart IChartArea.Chart => throw new NotSupportedException();
+
+		public override void Load(SettingsStorage storage)
+		{
+			base.Load(storage);
+
+			Title = storage.GetValue<string>(nameof(Title));
+			Height = storage.GetValue<double>(nameof(Height));
+			XAxisType = storage.GetValue<ChartAxisType>(nameof(XAxisType));
+		}
 	}
 
 	private class DummyAxis : DummyPersistable, IChartAxis
 	{
-		IChartArea IChartAxis.ChartArea => throw new NotImplementedException();
+		IChartArea IChartAxis.ChartArea => throw new NotSupportedException();
 
-		string IChartAxis.Id { get; set; }
-		bool IChartAxis.IsVisible { get; set; }
-		string IChartAxis.Title { get; set; }
-		string IChartAxis.Group { get; set; }
-		bool IChartAxis.SwitchAxisLocation { get; set; }
-		ChartAxisType IChartAxis.AxisType { get; set; }
-		bool IChartAxis.AutoRange { get; set; }
-		bool IChartAxis.FlipCoordinates { get; set; }
-		bool IChartAxis.DrawMajorTicks { get; set; }
-		bool IChartAxis.DrawMajorGridLines { get; set; }
-		bool IChartAxis.DrawMinorTicks { get; set; }
-		bool IChartAxis.DrawMinorGridLines { get; set; }
-		bool IChartAxis.DrawLabels { get; set; }
-		string IChartAxis.TextFormatting { get; set; }
-		string IChartAxis.SubDayTextFormatting { get; set; }
-		TimeZoneInfo IChartAxis.TimeZone { get; set; }
+		public string Id { get; set; }
+		public bool IsVisible { get; set; }
+		public string Title { get; set; }
+		public string Group { get; set; }
+		public bool SwitchAxisLocation { get; set; }
+		public ChartAxisType AxisType { get; set; }
+		public bool AutoRange { get; set; }
+		public bool FlipCoordinates { get; set; }
+		public bool DrawMajorTicks { get; set; }
+		public bool DrawMajorGridLines { get; set; }
+		public bool DrawMinorTicks { get; set; }
+		public bool DrawMinorGridLines { get; set; }
+		public bool DrawLabels { get; set; }
+		public string TextFormatting { get; set; }
+		public string SubDayTextFormatting { get; set; }
+		public TimeZoneInfo TimeZone { get; set; }
 
 		void INotifyPropertyChangedEx.NotifyPropertyChanged(string propertyName)
-			=> throw new NotImplementedException();
+			=> throw new NotSupportedException();
+
+		public override void Load(SettingsStorage storage)
+		{
+			base.Load(storage);
+
+			Id = storage.GetValue<string>(nameof(Id));
+			Title = storage.GetValue<string>(nameof(Title));
+			IsVisible = storage.GetValue<bool>(nameof(IsVisible));
+			Group = storage.GetValue<string>(nameof(Group));
+			AutoRange = storage.GetValue<bool>(nameof(AutoRange));
+			DrawMinorTicks = storage.GetValue<bool>(nameof(DrawMinorTicks));
+			DrawMajorTicks = storage.GetValue<bool>(nameof(DrawMajorTicks));
+			DrawMajorGridLines = storage.GetValue<bool>(nameof(DrawMajorGridLines));
+			DrawMinorGridLines = storage.GetValue<bool>(nameof(DrawMinorGridLines));
+			DrawLabels = storage.GetValue<bool>(nameof(DrawLabels));
+			TextFormatting = storage.GetValue<string>(nameof(TextFormatting));
+			SubDayTextFormatting = storage.GetValue(nameof(SubDayTextFormatting), SubDayTextFormatting);
+			SwitchAxisLocation = storage.GetValue<bool>(nameof(SwitchAxisLocation));
+			AxisType = storage.GetValue<ChartAxisType>(nameof(AxisType));
+			TimeZone = storage.GetValue<string>(nameof(TimeZone)).To<TimeZoneInfo>() ?? TimeZoneInfo.Local;
+		}
 	}
 
 	private class DummyBandElement : DummyElement, IChartBandElement
 	{
 		ChartIndicatorDrawStyles IChartBandElement.Style { get; set; }
-		IChartLineElement IChartBandElement.Line1 => throw new NotImplementedException();
-		IChartLineElement IChartBandElement.Line2 => throw new NotImplementedException();
+		IChartLineElement IChartBandElement.Line1 => throw new NotSupportedException();
+		IChartLineElement IChartBandElement.Line2 => throw new NotSupportedException();
 	}
 
-	private class DummyBubbleElement : DummyElement, IChartLineElement
+	private class DummyBubbleElement : DummyLineElement
 	{
-		Color IChartLineElement.Color { get; set; }
-		Color IChartLineElement.AdditionalColor { get; set; }
-		int IChartLineElement.StrokeThickness { get; set; }
-		bool IChartLineElement.AntiAliasing { get; set; }
-		ChartIndicatorDrawStyles IChartLineElement.Style { get; set; }
-		bool IChartLineElement.ShowAxisMarker { get; set; }
 	}
 
 	private class DummyLineElement : DummyElement, IChartLineElement
 	{
-		Color IChartLineElement.Color { get; set; }
-		Color IChartLineElement.AdditionalColor { get; set; }
-		int IChartLineElement.StrokeThickness { get; set; }
-		bool IChartLineElement.AntiAliasing { get; set; }
-		ChartIndicatorDrawStyles IChartLineElement.Style { get; set; }
-		bool IChartLineElement.ShowAxisMarker { get; set; }
+		public Color Color { get; set; }
+		public Color AdditionalColor { get; set; }
+		public int StrokeThickness { get; set; }
+		public bool AntiAliasing { get; set; }
+		public ChartIndicatorDrawStyles Style { get; set; }
+		public bool ShowAxisMarker { get; set; }
+
+		public override void Load(SettingsStorage storage)
+		{
+			base.Load(storage);
+
+			//Color = storage.GetValue<int>(nameof(Color)).ToColor();
+			//AdditionalColor = storage.GetValue<int>(nameof(AdditionalColor)).ToColor();
+			StrokeThickness = storage.GetValue<int>(nameof(StrokeThickness));
+			AntiAliasing = storage.GetValue<bool>(nameof(AntiAliasing));
+			Style = storage.GetValue<ChartIndicatorDrawStyles>(nameof(Style));
+			ShowAxisMarker = storage.GetValue<bool>(nameof(ShowAxisMarker));
+		}
 	}
 
 	private class DummyCandleElement : DummyElement, IChartCandleElement
 	{
-		ChartCandleDrawStyles IChartCandleElement.DrawStyle { get; set; }
-		Color IChartCandleElement.DownFillColor { get; set; }
-		Color IChartCandleElement.UpFillColor { get; set; }
-		Color IChartCandleElement.DownBorderColor { get; set; }
-		Color IChartCandleElement.UpBorderColor { get; set; }
-		int IChartCandleElement.StrokeThickness { get; set; }
-		bool IChartCandleElement.AntiAliasing { get; set; }
-		Color IChartCandleElement.LineColor { get; set; }
-		Color IChartCandleElement.AreaColor { get; set; }
-		bool IChartCandleElement.ShowAxisMarker { get; set; }
+		public ChartCandleDrawStyles DrawStyle { get; set; }
+		public Color DownFillColor { get; set; }
+		public Color UpFillColor { get; set; }
+		public Color DownBorderColor { get; set; }
+		public Color UpBorderColor { get; set; }
+		public int StrokeThickness { get; set; }
+		public bool AntiAliasing { get; set; }
+		public Color LineColor { get; set; }
+		public Color AreaColor { get; set; }
+		public bool ShowAxisMarker { get; set; }
 		Func<DateTimeOffset, bool, bool, Color?> IChartCandleElement.Colorer { get; set; }
-		int IChartCandleElement.Timeframe2Multiplier { get; set; }
-		int IChartCandleElement.Timeframe3Multiplier { get; set; }
-		Color IChartCandleElement.FontColor { get; set; }
-		Color IChartCandleElement.Timeframe2Color { get; set; }
-		Color IChartCandleElement.Timeframe2FrameColor { get; set; }
-		Color IChartCandleElement.Timeframe3Color { get; set; }
-		Color IChartCandleElement.MaxVolumeColor { get; set; }
-		Color IChartCandleElement.ClusterLineColor { get; set; }
-		Color IChartCandleElement.ClusterSeparatorLineColor { get; set; }
-		Color IChartCandleElement.ClusterTextColor { get; set; }
-		Color IChartCandleElement.ClusterColor { get; set; }
-		Color IChartCandleElement.ClusterMaxColor { get; set; }
-		bool IChartCandleElement.ShowHorizontalVolumes { get; set; }
-		bool IChartCandleElement.LocalHorizontalVolumes { get; set; }
-		double IChartCandleElement.HorizontalVolumeWidthFraction { get; set; }
-		Color IChartCandleElement.HorizontalVolumeColor { get; set; }
-		Color IChartCandleElement.HorizontalVolumeFontColor { get; set; }
+		public int Timeframe2Multiplier { get; set; }
+		public int Timeframe3Multiplier { get; set; }
+		public Color FontColor { get; set; }
+		public Color Timeframe2Color { get; set; }
+		public Color Timeframe2FrameColor { get; set; }
+		public Color Timeframe3Color { get; set; }
+		public Color MaxVolumeColor { get; set; }
+		public Color ClusterLineColor { get; set; }
+		public Color ClusterSeparatorLineColor { get; set; }
+		public Color ClusterTextColor { get; set; }
+		public Color ClusterColor { get; set; }
+		public Color ClusterMaxColor { get; set; }
+		public bool ShowHorizontalVolumes { get; set; }
+		public bool LocalHorizontalVolumes { get; set; }
+		public double HorizontalVolumeWidthFraction { get; set; }
+		public Color HorizontalVolumeColor { get; set; }
+		public Color HorizontalVolumeFontColor { get; set; }
+
+		public override void Load(SettingsStorage storage)
+		{
+			base.Load(storage);
+
+			DrawStyle = storage.GetValue<ChartCandleDrawStyles>(nameof(DrawStyle));
+
+			UpFillColor = storage.GetValue<int>(nameof(UpFillColor)).ToColor();
+			UpBorderColor = storage.GetValue<int>(nameof(UpBorderColor)).ToColor();
+			DownFillColor = storage.GetValue<int>(nameof(DownFillColor)).ToColor();
+			DownBorderColor = storage.GetValue<int>(nameof(DownBorderColor)).ToColor();
+			LineColor = storage.GetValue<int>(nameof(LineColor)).ToColor();
+			AreaColor = storage.GetValue<int>(nameof(AreaColor)).ToColor();
+
+			DrawStyle = storage.GetValue<ChartCandleDrawStyles>(nameof(DrawStyle));
+			StrokeThickness = storage.GetValue<int>(nameof(StrokeThickness));
+			AntiAliasing = storage.GetValue<bool>(nameof(AntiAliasing));
+			ShowAxisMarker = storage.GetValue<bool>(nameof(ShowAxisMarker));
+
+			Timeframe2Multiplier = storage.GetValue<int>(nameof(Timeframe2Multiplier));
+			Timeframe3Multiplier = storage.GetValue<int>(nameof(Timeframe3Multiplier));
+			FontColor = storage.GetValue<int>(nameof(FontColor)).ToColor();
+			Timeframe2Color = storage.GetValue<int>(nameof(Timeframe2Color)).ToColor();
+			Timeframe2FrameColor = storage.GetValue<int>(nameof(Timeframe2FrameColor)).ToColor();
+			Timeframe3Color = storage.GetValue<int>(nameof(Timeframe3Color)).ToColor();
+			MaxVolumeColor = storage.GetValue<int>(nameof(MaxVolumeColor)).ToColor();
+			ClusterSeparatorLineColor = storage.GetValue<int>(nameof(ClusterSeparatorLineColor)).ToColor();
+			ClusterLineColor = storage.GetValue<int>(nameof(ClusterLineColor)).ToColor();
+			ClusterTextColor = storage.GetValue<int>(nameof(ClusterTextColor)).ToColor();
+			ClusterColor = storage.GetValue<int>(nameof(ClusterColor)).ToColor();
+			ClusterMaxColor = storage.GetValue<int>(nameof(ClusterMaxColor)).ToColor();
+			ShowHorizontalVolumes = storage.GetValue<bool>(nameof(ShowHorizontalVolumes));
+			LocalHorizontalVolumes = storage.GetValue<bool>(nameof(LocalHorizontalVolumes));
+			HorizontalVolumeWidthFraction = storage.GetValue<double>(nameof(HorizontalVolumeWidthFraction));
+			HorizontalVolumeColor = storage.GetValue<int>(nameof(HorizontalVolumeColor)).ToColor();
+			HorizontalVolumeFontColor = storage.GetValue<int>(nameof(HorizontalVolumeFontColor)).ToColor();
+		}
 	}
 
 	private class DummyTransactionElement : DummyElement, IChartTransactionElement
 	{
-		Color IChartTransactionElement.BuyColor { get; set; }
-		Color IChartTransactionElement.BuyStrokeColor { get; set; }
-		Color IChartTransactionElement.SellColor { get; set; }
-		Color IChartTransactionElement.SellStrokeColor { get; set; }
-		bool IChartTransactionElement.UseAltIcon { get; set; }
-		double IChartTransactionElement.DrawSize { get; set; }
+		public Color BuyColor { get; set; }
+		public Color BuyStrokeColor { get; set; }
+		public Color SellColor { get; set; }
+		public Color SellStrokeColor { get; set; }
+		public bool UseAltIcon { get; set; }
+		public double DrawSize { get; set; }
+
+		/// <inheritdoc />
+		public override void Load(SettingsStorage storage)
+		{
+			base.Load(storage);
+
+			BuyColor = storage.GetValue<int>(nameof(BuyColor)).ToColor();
+			BuyStrokeColor = storage.GetValue<int>(nameof(BuyStrokeColor)).ToColor();
+			SellColor = storage.GetValue<int>(nameof(SellColor)).ToColor();
+			SellStrokeColor = storage.GetValue<int>(nameof(SellStrokeColor)).ToColor();
+
+			UseAltIcon = storage.GetValue<bool>(nameof(UseAltIcon));
+			DrawSize = storage.GetValue<double>(nameof(DrawSize));
+		}
 	}
 
 	private class DummyOrderElement : DummyTransactionElement, IChartOrderElement
