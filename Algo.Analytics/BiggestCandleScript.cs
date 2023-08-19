8eed677d1bf4aa6975233d8cb11b0abfeed33771
mikasoukhov <mika@stocksharp.com>
2023-08-19 22:10:09 +0300

    Fix commit e47711c8d9e21f3b936179fac4c3aa58f7303898

--- Algo.Analytics/BiggestCandleScript.cs
+++ Algo.Analytics/BiggestCandleScript.cs
@@ -32,8 +32,8 @@
 			}
 
 			// draw series on chart
-			priceChart.Append("prices", bigPriceCandles.Select(c => c.OpenTime), bigPriceCandles.Select(c => c.GetMiddlePrice(null)), bigPriceCandles.Select(c => c.GetLength()), ChartIndicatorDrawStyles.Bubble);
-			priceChart.Append("prices", bigVolCandles.Select(c => c.OpenTime), bigPriceCandles.Select(c => c.GetMiddlePrice(null)), bigVolCandles.Select(c => c.TotalVolume), ChartIndicatorDrawStyles.Bubble);
+			priceChart.Append("prices", bigPriceCandles.Select(c => c.OpenTime), bigPriceCandles.Select(c => c.GetMiddlePrice(null)), bigPriceCandles.Select(c => c.GetLength()), color: Color.Green);
+			priceChart.Append("prices", bigVolCandles.Select(c => c.OpenTime), bigPriceCandles.Select(c => c.GetMiddlePrice(null)), bigVolCandles.Select(c => c.TotalVolume), color: Color.Blue);
 
 			return Task.CompletedTask;
 		}
