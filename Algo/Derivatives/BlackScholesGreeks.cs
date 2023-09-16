5cea0f7fe9f7b4559ca3ef8ef286dc7d9ced31c2
mikasoukhov <mika@stocksharp.com>
2023-09-16 13:33:23 +0300

    BlackScholesGreeks enum.

---
+++ Algo/Derivatives/BlackScholesGreeks.cs
@@ -0,0 +1,53 @@
+﻿namespace StockSharp.Algo.Derivatives;
+
+using System.ComponentModel.DataAnnotations;
+
+using StockSharp.Localization;
+
+/// <summary>
+/// Black-Scholes "greeks".
+/// </summary>
+public enum BlackScholesGreeks
+{
+	/// <summary>
+	/// Delta.
+	/// </summary>
+	[Display(ResourceType = typeof(LocalizedStrings), Name = LocalizedStrings.DeltaKey)]
+	Delta,
+
+	/// <summary>
+	/// Gamma.
+	/// </summary>
+	[Display(ResourceType = typeof(LocalizedStrings), Name = LocalizedStrings.GammaKey)]
+	Gamma,
+
+	/// <summary>
+	/// Vega.
+	/// </summary>
+	[Display(ResourceType = typeof(LocalizedStrings), Name = LocalizedStrings.VegaKey)]
+	Vega,
+
+	/// <summary>
+	/// Theta.
+	/// </summary>
+	[Display(ResourceType = typeof(LocalizedStrings), Name = LocalizedStrings.ThetaKey)]
+	Theta,
+
+	/// <summary>
+	/// Rho.
+	/// </summary>
+	[Display(ResourceType = typeof(LocalizedStrings), Name = LocalizedStrings.RhoKey)]
+	Rho,
+
+	/// <summary>
+	/// Premium.
+	/// </summary>
+	[Display(ResourceType = typeof(LocalizedStrings), Name = LocalizedStrings.PremiumKey)]
+	Premium,
+
+	/// <summary>
+	/// Premium.
+	/// </summary>
+	[Display(ResourceType = typeof(LocalizedStrings), Name = LocalizedStrings.IVKey)]
+	IV,
+}
