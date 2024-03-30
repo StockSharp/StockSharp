2b302ab12c4dbf1d822842d9bb05541dbd00c967
mikasoukhov <mika@stocksharp.com>
2024-03-30 12:51:53 +0300

    DESIGNER-214 Fix deserialize cloud reports with math based indicators.

---
+++ Algo/Indicators/PassThroughIndicator.cs
@@ -0,0 +1,28 @@
+﻿namespace StockSharp.Algo.Indicators;
+
+using System.ComponentModel.DataAnnotations;
+
+using Ecng.ComponentModel;
+
+using StockSharp.Localization;
+
+/// <summary>
+/// Pass through indicator.
+/// </summary>
+[Display(
+	ResourceType = typeof(LocalizedStrings),
+	Name = LocalizedStrings.StubKey,
+	Description = LocalizedStrings.StubIndicatorKey)]
+[Doc("topics/IndicatorPassThrough.html")]
+public class PassThroughIndicator : BaseIndicator
+{
+	/// <inheritdoc/>
+	protected override IIndicatorValue OnProcess(IIndicatorValue input)
+	{
+		IsFormed |= !input.IsEmpty;
+
+		return input.IsEmpty
+			? new DecimalIndicatorValue(this)
+			: new DecimalIndicatorValue(this, input.GetValue<decimal>());
+	}
+}
