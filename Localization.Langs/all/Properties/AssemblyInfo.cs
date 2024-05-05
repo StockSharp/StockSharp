f9743402ff8195d1301cc91241e018554877dd42
mikasoukhov <mika@stocksharp.com>
2024-05-05 10:55:12 +0300

    Fix commit 66b891ac42c748069f079e10d0fe3951e727235d

---
+++ Localization.Langs/all/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("All Languages Assembly")]
+[assembly: AssemblyDescription("Single reference point for all localization.")]
