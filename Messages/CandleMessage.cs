9389604b7e878d0c650c17e0d53e82fd8dfdfa22
mikasoukhov <mika@stocksharp.com>
2024-05-16 22:30:51 +0300

    CandleMessage. ToString fix

--- Messages/CandleMessage.cs
+++ Messages/CandleMessage.cs
@@ -353,7 +353,7 @@ namespace StockSharp.Messages
 		/// <inheritdoc />
 		public override string ToString()
 		{
-			var str = $"{Type},Sec={SecurityId},A={DataType.Arg},T={OpenTime:yyyy/MM/dd HH:mm:ss.fff},O={OpenPrice},H={HighPrice},L={LowPrice},C={ClosePrice},V={TotalVolume},S={State},TransId={OriginalTransactionId}";
+			var str = $"{Type},Sec={SecurityId},A={DataType?.Arg},T={OpenTime:yyyy/MM/dd HH:mm:ss.fff},O={OpenPrice},H={HighPrice},L={LowPrice},C={ClosePrice},V={TotalVolume},S={State},TransId={OriginalTransactionId}";
 
 			if (SeqNum != default)
 				str += $",SQ={SeqNum}";
