cd8772bfd5d89dcb960d48be355af3f1ee5f1ad1
mikasoukhov <mika@stocksharp.com>
2024-06-12 15:14:42 +0300

    IOAuthProvider moved to Ecng.Net

--- Messages/IMessageAdapter.cs
+++ Messages/IMessageAdapter.cs
@@ -370,18 +370,4 @@ namespace StockSharp.Messages
 		/// </summary>
 		string TargetCompId { get; set; }
 	}
-
-	/// <summary>
-	/// Provider for OAuth.
-	/// </summary>
-	public interface IOAuthProvider
-	{
-		/// <summary>
-		/// Request token.
-		/// </summary>
-		/// <param name="socialId"></param>
-		/// <param name="cancellationToken"></param>
-		/// <returns></returns>
-		Task<(SecureString token, DateTime? till)> RequestToken(long socialId, CancellationToken cancellationToken);
-	}
 }
