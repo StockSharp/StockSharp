08f61c30413b509881784166166f43656e0893db
mikasoukhov <mika@stocksharp.com>
2024-08-27 21:39:29 +0300

    AccumulationDistributionLine indicator

---
+++ Algo/Indicators/AccumulationDistributionLine.cs
@@ -0,0 +1,41 @@
+﻿namespace StockSharp.Algo.Indicators;
+
+/// <summary>
+/// Accumulation/Distribution Line (A/D Line).
+/// </summary>
+[Display(
+	ResourceType = typeof(LocalizedStrings),
+	Name = LocalizedStrings.ADLKey,
+	Description = LocalizedStrings.AccumulationDistributionLineKey)]
+[IndicatorIn(typeof(CandleIndicatorValue))]
+[Doc("topics/indicators/accumulation_distribution_line.html")]
+public class AccumulationDistributionLine : BaseIndicator
+{
+	private decimal _adLine;
+
+	/// <inheritdoc />
+	protected override IIndicatorValue OnProcess(IIndicatorValue input)
+	{
+		var (_, high, low, close, volume) = input.GetOhlcv();
+
+		if (high != low)
+		{
+			var mfm = ((close - low) - (high - close)) / (high - low);
+			var mfv = mfm * volume;
+			_adLine += mfv;
+		}
+
+		if (input.IsFinal)
+			IsFormed = true;
+
+		return new DecimalIndicatorValue(this, _adLine, input.Time);
+	}
+
+	/// <inheritdoc />
+	public override void Reset()
+	{
+		_adLine = 0;
+
+		base.Reset();
+	}
+}
