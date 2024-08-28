5283abd04f819b887366d4723e10b2177b3ca1c2
mikasoukhov <mika@stocksharp.com>
2024-08-28 09:33:50 +0300

    BalanceVolume indicator.

---
+++ Algo/Indicators/BalanceVolume.cs
@@ -0,0 +1,62 @@
+﻿namespace StockSharp.Algo.Indicators;
+
+/// <summary>
+/// Balance Volume indicator.
+/// </summary>
+[Display(
+	ResourceType = typeof(LocalizedStrings),
+	Name = LocalizedStrings.BVKey,
+	Description = LocalizedStrings.BalanceVolumeKey)]
+[IndicatorIn(typeof(CandleIndicatorValue))]
+[Doc("topics/indicators/balance_volume.html")]
+public class BalanceVolume : BaseIndicator
+{
+	private decimal _prevClose;
+	private decimal _cumulativeBalanceVolume;
+
+	/// <summary>
+	/// Initializes a new instance of the <see cref="BalanceVolume"/>.
+	/// </summary>
+	public BalanceVolume()
+	{
+	}
+
+	/// <inheritdoc />
+	public override IndicatorMeasures Measure => IndicatorMeasures.Volume;
+
+	/// <inheritdoc />
+	protected override IIndicatorValue OnProcess(IIndicatorValue input)
+	{
+		var (_, _, _, close, volume) = input.GetOhlcv();
+
+		if (_prevClose == 0)
+		{
+			_prevClose = close;
+			return new DecimalIndicatorValue(this, input.Time);
+		}
+
+		decimal cumulativeBalanceVolume;
+
+		var balanceVolume = close > _prevClose ? volume : (close < _prevClose ? -volume : 0);
+
+		if (input.IsFinal)
+		{
+			_cumulativeBalanceVolume += balanceVolume;
+			_prevClose = close;
+			cumulativeBalanceVolume = _cumulativeBalanceVolume;
+			IsFormed = true;
+		}
+		else
+			cumulativeBalanceVolume = _cumulativeBalanceVolume + balanceVolume;
+
+		return new DecimalIndicatorValue(this, cumulativeBalanceVolume, input.Time);
+	}
+
+	/// <inheritdoc />
+	public override void Reset()
+	{
+		_prevClose = 0;
+		_cumulativeBalanceVolume = 0;
+		base.Reset();
+	}
+}
