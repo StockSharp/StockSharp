836c0f81fc472add90a51929a0c921e0b7b24240
mikasoukhov <mika@stocksharp.com>
2024-09-03 19:51:07 +0300

    RainbowCharts indicator.

---
+++ Algo/Indicators/RainbowCharts.cs
@@ -0,0 +1,67 @@
+﻿namespace StockSharp.Algo.Indicators;
+
+/// <summary>
+/// Rainbow Charts.
+/// </summary>
+[Display(
+	ResourceType = typeof(LocalizedStrings),
+	Name = LocalizedStrings.RCKey,
+	Description = LocalizedStrings.RainbowChartsKey)]
+[IndicatorIn(typeof(CandleIndicatorValue))]
+[Doc("topics/indicators/rainbow_charts.html")]
+public class RainbowCharts : BaseComplexIndicator
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="RainbowCharts"/>.
+	/// </summary>
+	public RainbowCharts()
+	{
+		Lines = 10;
+	}
+
+	private int _lines;
+
+	/// <summary>
+	/// Number of SMA lines.
+	/// </summary>
+	[Display(
+		ResourceType = typeof(LocalizedStrings),
+		Name = LocalizedStrings.SMAKey,
+		Description = LocalizedStrings.SimpleMovingAverageKey,
+		GroupName = LocalizedStrings.GeneralKey)]
+	public int Lines
+	{
+		get => _lines;
+		set
+		{
+			if (value < 1)
+				throw new ArgumentOutOfRangeException(nameof(value), value, LocalizedStrings.InvalidValue);
+
+			_lines = value;
+
+			ClearInner();
+
+			for (var i = 1; i < Lines - 1; i++)
+				AddInner(new SimpleMovingAverage { Length = i * 2 });
+
+			Reset();
+		}
+	}
+
+	/// <inheritdoc />
+	public override void Save(SettingsStorage storage)
+	{
+		base.Save(storage);
+		storage.SetValue(nameof(Lines), Lines);
+	}
+
+	/// <inheritdoc />
+	public override void Load(SettingsStorage storage)
+	{
+		base.Load(storage);
+		Lines = storage.GetValue<int>(nameof(Lines));
+	}
+
+	/// <inheritdoc />
+	public override string ToString() => $"{base.ToString()} L={Lines}";
+}
