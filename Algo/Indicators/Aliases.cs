0ca996ea05a9405195a0aab94b60454636b244c4
mikasoukhov <mika@stocksharp.com>
2024-09-03 22:24:30 +0300

    Indicators aliases

--- Algo/Indicators/Aliases.cs
+++ Algo/Indicators/Aliases.cs
@@ -527,3 +527,891 @@ public class ZZ : ZigZag
 	/// </summary>
 	public ZZ() { }
 }
+
+/// <summary>
+/// <see cref="SchaffTrendCycle"/> alias.
+/// </summary>
+[Browsable(false)]
+public class STC : SchaffTrendCycle
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="STC"/>.
+	/// </summary>
+	public STC() { }
+}
+
+/// <summary>
+/// <see cref="KeltnerChannels"/> alias.
+/// </summary>
+[Browsable(false)]
+public class KC : KeltnerChannels
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="KC"/>.
+	/// </summary>
+	public KC() { }
+}
+
+/// <summary>
+/// <see cref="ChaikinMoneyFlow"/> alias.
+/// </summary>
+[Browsable(false)]
+public class CMF : ChaikinMoneyFlow
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="CMF"/>.
+	/// </summary>
+	public CMF() { }
+}
+
+/// <summary>
+/// <see cref="PivotPoints"/> alias.
+/// </summary>
+[Browsable(false)]
+public class PP : PivotPoints
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="PP"/>.
+	/// </summary>
+	public PP() { }
+}
+
+/// <summary>
+/// <see cref="DonchianChannels"/> alias.
+/// </summary>
+[Browsable(false)]
+public class DC : DonchianChannels
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="DC"/>.
+	/// </summary>
+	public DC() { }
+}
+
+/// <summary>
+/// <see cref="ForceIndex"/> alias.
+/// </summary>
+[Browsable(false)]
+public class FI : ForceIndex
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="FI"/>.
+	/// </summary>
+	public FI() { }
+}
+
+/// <summary>
+/// <see cref="VortexIndicator"/> alias.
+/// </summary>
+[Browsable(false)]
+public class VI : VortexIndicator
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="VI"/>.
+	/// </summary>
+	public VI() { }
+}
+
+/// <summary>
+/// <see cref="KnowSureThing"/> alias.
+/// </summary>
+[Browsable(false)]
+public class KST : KnowSureThing
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="KST"/>.
+	/// </summary>
+	public KST() { }
+}
+
+/// <summary>
+/// <see cref="OnBalanceVolume"/> alias.
+/// </summary>
+[Browsable(false)]
+public class OBV : OnBalanceVolume
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="OBV"/>.
+	/// </summary>
+	public OBV() { }
+}
+
+/// <summary>
+/// <see cref="SineWave"/> alias.
+/// </summary>
+[Browsable(false)]
+public class SW : SineWave
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="SW"/>.
+	/// </summary>
+	public SW() { }
+}
+
+/// <summary>
+/// <see cref="AdaptiveLaguerreFilter"/> alias.
+/// </summary>
+[Browsable(false)]
+public class ALF : AdaptiveLaguerreFilter
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="ALF"/>.
+	/// </summary>
+	public ALF() { }
+}
+
+/// <summary>
+/// <see cref="ApprovalFlowIndex"/> alias.
+/// </summary>
+[Browsable(false)]
+public class AFI : ApprovalFlowIndex
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="AFI"/>.
+	/// </summary>
+	public AFI() { }
+}
+
+/// <summary>
+/// <see cref="BalanceVolume"/> alias.
+/// </summary>
+[Browsable(false)]
+public class BV : BalanceVolume
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="BV"/>.
+	/// </summary>
+	public BV() { }
+}
+
+/// <summary>
+/// <see cref="CenterOfGravityOscillator"/> alias.
+/// </summary>
+[Browsable(false)]
+public class CGO : CenterOfGravityOscillator
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="CGO"/>.
+	/// </summary>
+	public CGO() { }
+}
+
+/// <summary>
+/// <see cref="CompositeMomentum"/> alias.
+/// </summary>
+[Browsable(false)]
+public class CM : CompositeMomentum
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="CM"/>.
+	/// </summary>
+	public CM() { }
+}
+
+/// <summary>
+/// <see cref="ConstanceBrownCompositeIndex"/> alias.
+/// </summary>
+[Browsable(false)]
+public class CBCI : ConstanceBrownCompositeIndex
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="CBCI"/>.
+	/// </summary>
+	public CBCI() { }
+}
+
+/// <summary>
+/// <see cref="DetrendedSyntheticPrice"/> alias.
+/// </summary>
+[Browsable(false)]
+public class DSP : DetrendedSyntheticPrice
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="DSP"/>.
+	/// </summary>
+	public DSP() { }
+}
+
+/// <summary>
+/// <see cref="DynamicZonesRSI"/> alias.
+/// </summary>
+[Browsable(false)]
+public class DZRSI : DynamicZonesRSI
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="DZRSI"/>.
+	/// </summary>
+	public DZRSI() { }
+}
+
+/// <summary>
+/// <see cref="ElderImpulseSystem"/> alias.
+/// </summary>
+[Browsable(false)]
+public class EIS : ElderImpulseSystem
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="EIS"/>.
+	/// </summary>
+	public EIS() { }
+}
+
+/// <summary>
+/// <see cref="FiniteVolumeElement"/> alias.
+/// </summary>
+[Browsable(false)]
+public class FVE : FiniteVolumeElement
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="FVE"/>.
+	/// </summary>
+	public FVE() { }
+}
+
+/// <summary>
+/// <see cref="HarmonicOscillator"/> alias.
+/// </summary>
+[Browsable(false)]
+public class HO : HarmonicOscillator
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="HO"/>.
+	/// </summary>
+	public HO() { }
+}
+
+/// <summary>
+/// <see cref="HighLowIndex"/> alias.
+/// </summary>
+[Browsable(false)]
+public class HLI : HighLowIndex
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="HLI"/>.
+	/// </summary>
+	public HLI() { }
+}
+
+/// <summary>
+/// <see cref="HistoricalVolatilityRatio"/> alias.
+/// </summary>
+[Browsable(false)]
+public class HVR : HistoricalVolatilityRatio
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="HVR"/>.
+	/// </summary>
+	public HVR() { }
+}
+
+/// <summary>
+/// <see cref="KaufmanEfficiencyRatio"/> alias.
+/// </summary>
+[Browsable(false)]
+public class KER : KaufmanEfficiencyRatio
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="KER"/>.
+	/// </summary>
+	public KER() { }
+}
+
+/// <summary>
+/// <see cref="LunarPhase"/> alias.
+/// </summary>
+[Browsable(false)]
+public class LP : LunarPhase
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="LP"/>.
+	/// </summary>
+	public LP() { }
+}
+
+/// <summary>
+/// <see cref="MomentumPinball"/> alias.
+/// </summary>
+[Browsable(false)]
+public class MP : MomentumPinball
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="MP"/>.
+	/// </summary>
+	public MP() { }
+}
+
+/// <summary>
+/// <see cref="MovingAverageCrossover"/> alias.
+/// </summary>
+[Browsable(false)]
+public class MAC : MovingAverageCrossover
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="MAC"/>.
+	/// </summary>
+	public MAC() { }
+}
+
+/// <summary>
+/// <see cref="OnBalanceVolumeMean"/> alias.
+/// </summary>
+[Browsable(false)]
+public class OBVM : OnBalanceVolumeMean
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="OBVM"/>.
+	/// </summary>
+	public OBVM() { }
+}
+
+/// <summary>
+/// <see cref="OscillatorOfMovingAverage"/> alias.
+/// </summary>
+[Browsable(false)]
+public class OMA : OscillatorOfMovingAverage
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="OMA"/>.
+	/// </summary>
+	public OMA() { }
+}
+
+/// <summary>
+/// <see cref="DemandIndex"/> alias.
+/// </summary>
+[Browsable(false)]
+public class DI : DemandIndex
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="DI"/>.
+	/// </summary>
+	public DI() { }
+}
+
+/// <summary>
+/// <see cref="DisparityIndex"/> alias.
+/// </summary>
+[Browsable(false)]
+public class DPI : DisparityIndex
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="DPI"/>.
+	/// </summary>
+	public DPI() { }
+}
+
+/// <summary>
+/// <see cref="EhlersFisherTransform"/> alias.
+/// </summary>
+[Browsable(false)]
+public class EFT : EhlersFisherTransform
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="EFT"/>.
+	/// </summary>
+	public EFT() { }
+}
+
+/// <summary>
+/// <see cref="ForecastOscillator"/> alias.
+/// </summary>
+[Browsable(false)]
+public class FOSC : ForecastOscillator
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="FOSC"/>.
+	/// </summary>
+	public FOSC() { }
+}
+
+/// <summary>
+/// <see cref="IntradayMomentumIndex"/> alias.
+/// </summary>
+[Browsable(false)]
+public class IMI : IntradayMomentumIndex
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="IMI"/>.
+	/// </summary>
+	public IMI() { }
+}
+
+/// <summary>
+/// <see cref="TimeWeightedAveragePrice"/> alias.
+/// </summary>
+[Browsable(false)]
+public class TWAP : TimeWeightedAveragePrice
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="TWAP"/>.
+	/// </summary>
+	public TWAP() { }
+}
+
+/// <summary>
+/// <see cref="WilliamsAccumulationDistribution"/> alias.
+/// </summary>
+[Browsable(false)]
+public class WAD : WilliamsAccumulationDistribution
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="WAD"/>.
+	/// </summary>
+	public WAD() { }
+}
+
+/// <summary>
+/// <see cref="AdaptivePriceZone"/> alias.
+/// </summary>
+[Browsable(false)]
+public class APZ : AdaptivePriceZone
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="APZ"/>.
+	/// </summary>
+	public APZ() { }
+}
+
+/// <summary>
+/// <see cref="ArnaudLegouxMovingAverage"/> alias.
+/// </summary>
+[Browsable(false)]
+public class ALMA : ArnaudLegouxMovingAverage
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="ALMA"/>.
+	/// </summary>
+	public ALMA() { }
+}
+
+/// <summary>
+/// <see cref="BalanceOfMarketPower"/> alias.
+/// </summary>
+[Browsable(false)]
+public class BMP : BalanceOfMarketPower
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="BMP"/>.
+	/// </summary>
+	public BMP() { }
+}
+
+/// <summary>
+/// <see cref="BollingerPercentB"/> alias.
+/// </summary>
+[Browsable(false)]
+public class BBP : BollingerPercentB
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="BBP"/>.
+	/// </summary>
+	public BBP() { }
+}
+
+/// <summary>
+/// <see cref="ChandeKrollStop"/> alias.
+/// </summary>
+[Browsable(false)]
+public class CKS : ChandeKrollStop
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="CKS"/>.
+	/// </summary>
+	public CKS() { }
+}
+
+/// <summary>
+/// <see cref="ChoppinessIndex"/> alias.
+/// </summary>
+[Browsable(false)]
+public class CHOP : ChoppinessIndex
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="CHOP"/>.
+	/// </summary>
+	public CHOP() { }
+}
+
+/// <summary>
+/// <see cref="ConnorsRSI"/> alias.
+/// </summary>
+[Browsable(false)]
+public class CRSI : ConnorsRSI
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="CRSI"/>.
+	/// </summary>
+	public CRSI() { }
+}
+
+/// <summary>
+/// <see cref="ElliotWaveOscillator"/> alias.
+/// </summary>
+[Browsable(false)]
+public class EWO : ElliotWaveOscillator
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="EWO"/>.
+	/// </summary>
+	public EWO() { }
+}
+
+/// <summary>
+/// <see cref="EndpointMovingAverage"/> alias.
+/// </summary>
+[Browsable(false)]
+public class EPMA : EndpointMovingAverage
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="EPMA"/>.
+	/// </summary>
+	public EPMA() { }
+}
+
+/// <summary>
+/// <see cref="GopalakrishnanRangeIndex"/> alias.
+/// </summary>
+[Browsable(false)]
+public class GAPO : GopalakrishnanRangeIndex
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="GAPO"/>.
+	/// </summary>
+	public GAPO() { }
+}
+
+/// <summary>
+/// <see cref="IntradayIntensityIndex"/> alias.
+/// </summary>
+[Browsable(false)]
+public class III : IntradayIntensityIndex
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="III"/>.
+	/// </summary>
+	public III() { }
+}
+
+/// <summary>
+/// <see cref="KasePeakOscillator"/> alias.
+/// </summary>
+[Browsable(false)]
+public class KPO : KasePeakOscillator
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="KPO"/>.
+	/// </summary>
+	public KPO() { }
+}
+
+/// <summary>
+/// <see cref="LaguerreRSI"/> alias.
+/// </summary>
+[Browsable(false)]
+public class LRSI : LaguerreRSI
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="LRSI"/>.
+	/// </summary>
+	public LRSI() { }
+}
+
+/// <summary>
+/// <see cref="MarketMeannessIndex"/> alias.
+/// </summary>
+[Browsable(false)]
+public class MMI : MarketMeannessIndex
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="MMI"/>.
+	/// </summary>
+	public MMI() { }
+}
+
+/// <summary>
+/// <see cref="MomentumOfMovingAverage"/> alias.
+/// </summary>
+[Browsable(false)]
+public class MOMA : MomentumOfMovingAverage
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="MOMA"/>.
+	/// </summary>
+	public MOMA() { }
+}
+
+/// <summary>
+/// <see cref="MovingAverageRibbon"/> alias.
+/// </summary>
+[Browsable(false)]
+public class MAR : MovingAverageRibbon
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="MAR"/>.
+	/// </summary>
+	public MAR() { }
+}
+
+/// <summary>
+/// <see cref="NegativeVolumeIndex"/> alias.
+/// </summary>
+[Browsable(false)]
+public class NVI : NegativeVolumeIndex
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="NVI"/>.
+	/// </summary>
+	public NVI() { }
+}
+
+/// <summary>
+/// <see cref="FibonacciRetracement"/> alias.
+/// </summary>
+[Browsable(false)]
+public class FR : FibonacciRetracement
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="FR"/>.
+	/// </summary>
+	public FR() { }
+}
+
+/// <summary>
+/// <see cref="AccumulationDistributionLine"/> alias.
+/// </summary>
+[Browsable(false)]
+public class ADL : AccumulationDistributionLine
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="ADL"/>.
+	/// </summary>
+	public ADL() { }
+}
+
+/// <summary>
+/// <see cref="EaseOfMovement"/> alias.
+/// </summary>
+[Browsable(false)]
+public class EMV : EaseOfMovement
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="EMV"/>.
+	/// </summary>
+	public EMV() { }
+}
+
+/// <summary>
+/// <see cref="PercentagePriceOscillator"/> alias.
+/// </summary>
+[Browsable(false)]
+public class PPO : PercentagePriceOscillator
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="PPO"/>.
+	/// </summary>
+	public PPO() { }
+}
+
+/// <summary>
+/// <see cref="PercentageVolumeOscillator"/> alias.
+/// </summary>
+[Browsable(false)]
+public class PVO : PercentageVolumeOscillator
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="PVO"/>.
+	/// </summary>
+	public PVO() { }
+}
+
+/// <summary>
+/// <see cref="MassIndex"/> alias.
+/// </summary>
+[Browsable(false)]
+public class MI : MassIndex
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="MI"/>.
+	/// </summary>
+	public MI() { }
+}
+
+/// <summary>
+/// <see cref="TwiggsMoneyFlow"/> alias.
+/// </summary>
+[Browsable(false)]
+public class TMF : TwiggsMoneyFlow
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="TMF"/>.
+	/// </summary>
+	public TMF() { }
+}
+
+/// <summary>
+/// <see cref="RainbowCharts"/> alias.
+/// </summary>
+[Browsable(false)]
+public class RC : RainbowCharts
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="RC"/>.
+	/// </summary>
+	public RC() { }
+}
+
+/// <summary>
+/// <see cref="KlingerVolumeOscillator"/> alias.
+/// </summary>
+[Browsable(false)]
+public class KVO : KlingerVolumeOscillator
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="KVO"/>.
+	/// </summary>
+	public KVO() { }
+}
+
+/// <summary>
+/// <see cref="FractalAdaptiveMovingAverage"/> alias.
+/// </summary>
+[Browsable(false)]
+public class FRAMA : FractalAdaptiveMovingAverage
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="FRAMA"/>.
+	/// </summary>
+	public FRAMA() { }
+}
+
+/// <summary>
+/// <see cref="GuppyMultipleMovingAverage"/> alias.
+/// </summary>
+[Browsable(false)]
+public class GMMA : GuppyMultipleMovingAverage
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="GMMA"/>.
+	/// </summary>
+	public GMMA() { }
+}
+
+/// <summary>
+/// <see cref="PsychologicalLine"/> alias.
+/// </summary>
+[Browsable(false)]
+public class PSY : PsychologicalLine
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="PSY"/>.
+	/// </summary>
+	public PSY() { }
+}
+
+/// <summary>
+/// <see cref="McClellanOscillator"/> alias.
+/// </summary>
+[Browsable(false)]
+public class MCO : McClellanOscillator
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="MCO"/>.
+	/// </summary>
+	public MCO() { }
+}
+
+/// <summary>
+/// <see cref="McGinleyDynamic"/> alias.
+/// </summary>
+[Browsable(false)]
+public class MGD : McGinleyDynamic
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="MGD"/>.
+	/// </summary>
+	public MGD() { }
+}
+
+/// <summary>
+/// <see cref="T3MovingAverage"/> alias.
+/// </summary>
+[Browsable(false)]
+public class T3 : T3MovingAverage
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="T3"/>.
+	/// </summary>
+	public T3() { }
+}
+
+/// <summary>
+/// <see cref="WoodiesCCI"/> alias.
+/// </summary>
+[Browsable(false)]
+public class WCCI : WoodiesCCI
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="WCCI"/>.
+	/// </summary>
+	public WCCI() { }
+}
+
+/// <summary>
+/// <see cref="ZeroLagExponentialMovingAverage"/> alias.
+/// </summary>
+[Browsable(false)]
+public class ZLEMA : ZeroLagExponentialMovingAverage
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="ZLEMA"/>.
+	/// </summary>
+	public ZLEMA() { }
+}
+
+/// <summary>
+/// <see cref="BalanceOfPower"/> alias.
+/// </summary>
+[Browsable(false)]
+public class BOP : BalanceOfPower
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="BOP"/>.
+	/// </summary>
+	public BOP() { }
+}
+
+/// <summary>
+/// <see cref="PrettyGoodOscillator"/> alias.
+/// </summary>
+[Browsable(false)]
+public class PGO : PrettyGoodOscillator
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="PGO"/>.
+	/// </summary>
+	public PGO() { }
+}
+
+/// <summary>
+/// <see cref="RelativeMomentumIndex"/> alias.
+/// </summary>
+[Browsable(false)]
+public class RMI : RelativeMomentumIndex
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="RMI"/>.
+	/// </summary>
+	public RMI() { }
+}
+
+/// <summary>
+/// <see cref="WaveTrendOscillator"/> alias.
+/// </summary>
+[Browsable(false)]
+public class WTO : WaveTrendOscillator
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="WTO"/>.
+	/// </summary>
+	public WTO() { }
+}
