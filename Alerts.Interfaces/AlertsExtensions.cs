e649041e04dde227e5153f42fe8dbf7147a65050
mikasoukhov <mika@stocksharp.com>
2024-10-13 23:38:27 +0300

    TryFindChannel extension.

---
+++ Alerts.Interfaces/AlertsExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace StockSharp.Alerts;
+
+using Ecng.Configuration;
+
+/// <summary>
+/// Alert extensions.
+/// </summary>
+public static class AlertsExtensions
+{
+	/// <summary>
+	/// Try to find the channel by the specified identifier.
+	/// </summary>
+	/// <param name="channelId">Channel id.</param>
+	/// <returns>Channel.</returns>
+	public static ITelegramChannel TryFindChannel(this long channelId)
+		=> ConfigManager.TryGetService<IEnumerable<ITelegramChannel>>().FirstOrDefault(c => c.Id == channelId);
+}
