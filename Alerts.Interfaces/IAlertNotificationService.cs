2d7009b6c70ceb16bc494a4d0e35f76f7118c30f
mikasoukhov <mika@stocksharp.com>
2024-10-29 20:21:51 +0300

    AlertSchema. LogLevel property.

--- Alerts.Interfaces/IAlertNotificationService.cs
+++ Alerts.Interfaces/IAlertNotificationService.cs
@@ -10,12 +10,13 @@ public interface IAlertNotificationService : ILogSource
 	/// </summary>
 	/// <param name="type">Alert type.</param>
 	/// <param name="externalId">External ID.</param>
+	/// <param name="logLevel"><see cref="LogLevels"/></param>
 	/// <param name="caption">Signal header.</param>
 	/// <param name="message">Alert text.</param>
 	/// <param name="time">Creation time.</param>
 	/// <param name="cancellationToken"><see cref="CancellationToken"/>.</param>
 	/// <returns><see cref="ValueTask"/>.</returns>
-	ValueTask NotifyAsync(AlertNotifications type, long? externalId, string caption, string message, DateTimeOffset time, CancellationToken cancellationToken);
+	ValueTask NotifyAsync(AlertNotifications type, long? externalId, LogLevels logLevel, string caption, string message, DateTimeOffset time, CancellationToken cancellationToken);
 }
 
 /// <summary>
