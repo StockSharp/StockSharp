709e5da3e6ae0ead8ef45156abdf24e3e351439f
mikasoukhov <mika@stocksharp.com>
2025-01-24 17:59:06 +0300

    Code cleanup.

---
+++ Messages/UnitExtensions.cs
@@ -0,0 +1,105 @@
+﻿namespace StockSharp.Messages;
+
+/// <summary>
+/// Extension class for <see cref="Unit"/>.
+/// </summary>
+public static class UnitExtensions
+{
+	/// <summary>
+	/// Convert the <see cref="int"/> to percents.
+	/// </summary>
+	/// <param name="value"><see cref="int"/> value.</param>
+	/// <returns>Percents.</returns>
+	public static Unit Percents(this int value) => Percents((decimal)value);
+
+	/// <summary>
+	/// Convert the <see cref="long"/> to percents.
+	/// </summary>
+	/// <param name="value"><see cref="long"/> value.</param>
+	/// <returns>Percents.</returns>
+	public static Unit Percents(this long value) => Percents((decimal)value);
+
+	/// <summary>
+	/// Convert the <see cref="double"/> to percents.
+	/// </summary>
+	/// <param name="value"><see cref="double"/> value.</param>
+	/// <returns>Percents.</returns>
+	public static Unit Percents(this double value) => Percents((decimal)value);
+
+	/// <summary>
+	/// Convert the <see cref="decimal"/> to percents.
+	/// </summary>
+	/// <param name="value"><see cref="decimal"/> value.</param>
+	/// <returns>Percents.</returns>
+	public static Unit Percents(this decimal value) => new(value, UnitTypes.Percent);
+
+	/// <summary>
+	/// Convert string to <see cref="Unit"/>.
+	/// </summary>
+	/// <param name="str">String value of <see cref="Unit"/>.</param>
+	/// <param name="throwIfNull">Throw <see cref="ArgumentNullException"/> if the specified string is empty.</param>
+	/// <param name="getTypeValue">The handler returns a value associated with <see cref="Type"/> (price or volume steps).</param>
+	/// <returns>Object <see cref="Unit"/>.</returns>
+	public static Unit ToUnit(this string str, bool throwIfNull = true, Func<UnitTypes, decimal?> getTypeValue = null)
+	{
+		if (str.IsEmpty())
+		{
+			if (throwIfNull)
+				throw new ArgumentNullException(nameof(str));
+
+			return null;
+		}
+
+		var lastSymbol = str.Last();
+
+		if (char.IsDigit(lastSymbol))
+			return new Unit(str.To<decimal>(), UnitTypes.Absolute);
+
+		var value = str.Substring(0, str.Length - 1).To<decimal>();
+
+		UnitTypes type;
+
+		switch (char.ToLowerInvariant(lastSymbol))
+		{
+			case 'ш':
+			case 's':
+				if (getTypeValue is null)
+					throw new ArgumentNullException(nameof(getTypeValue));
+
+				type = UnitTypes.Step;
+				break;
+			case 'п':
+			case 'p':
+				if (getTypeValue is null)
+					throw new ArgumentNullException(nameof(getTypeValue));
+
+				type = UnitTypes.Point;
+				break;
+			case '%':
+				type = UnitTypes.Percent;
+				break;
+			case 'л':
+			case 'l':
+				type = UnitTypes.Limit;
+				break;
+			default:
+				throw new ArgumentException(LocalizedStrings.UnknownUnitMeasurement.Put(lastSymbol), nameof(str));
+		}
+
+		return new Unit(value, type, getTypeValue);
+	}
+
+	/// <summary>
+	/// Multiple <see cref="Unit.Value"/> on the specified times.
+	/// </summary>
+	/// <param name="unit">Unit.</param>
+	/// <param name="times">Multiply value.</param>
+	/// <returns>Result.</returns>
+	public static Unit Times(this Unit unit, int times)
+	{
+		if (unit is null)
+			throw new ArgumentNullException(nameof(unit));
+
+		return new Unit(unit.Value * times, unit.Type, unit.GetTypeValue);
+	}
+}
---
+++ Messages/Unit_Cast.cs
@@ -0,0 +1,224 @@
+﻿namespace StockSharp.Messages;
+
+partial class Unit
+{
+	/// <summary>
+	/// Cast <see cref="decimal"/> object to the type <see cref="Unit"/>.
+	/// </summary>
+	/// <param name="value"><see cref="decimal"/> value.</param>
+	/// <returns>Object <see cref="Unit"/>.</returns>
+	public static implicit operator Unit(decimal value) => new(value);
+
+	/// <summary>
+	/// Cast <see cref="int"/> object to the type <see cref="Unit"/>.
+	/// </summary>
+	/// <param name="value"><see cref="int"/> value.</param>
+	/// <returns>Object <see cref="Unit"/>.</returns>
+	public static implicit operator Unit(int value) => new(value);
+
+	/// <summary>
+	/// Cast object from <see cref="Unit"/> to <see cref="decimal"/>.
+	/// </summary>
+	/// <param name="unit">Object <see cref="Unit"/>.</param>
+	/// <returns><see cref="decimal"/> value.</returns>
+	public static explicit operator decimal(Unit unit)
+	{
+		if (unit is null)
+			throw new ArgumentNullException(nameof(unit));
+
+		switch (unit.Type)
+		{
+			case UnitTypes.Limit:
+			case UnitTypes.Absolute:
+				return unit.Value;
+			case UnitTypes.Percent:
+				throw new ArgumentException(LocalizedStrings.PercentagesConvert, nameof(unit));
+			case UnitTypes.Point:
+				var point = unit.GetTypeValue?.Invoke(unit.Type) ?? throw new InvalidOperationException(LocalizedStrings.PriceStepNotSpecified);
+
+				return unit.Value * point;
+			case UnitTypes.Step:
+				var step = unit.GetTypeValue?.Invoke(unit.Type) ?? throw new InvalidOperationException(LocalizedStrings.PriceStepNotSpecified);
+
+				return unit.Value * step;
+			default:
+				throw new ArgumentOutOfRangeException(nameof(unit), unit.Type, LocalizedStrings.InvalidValue);
+		}
+	}
+
+	/// <summary>
+	/// Cast object from <see cref="Unit"/> to nullable <see cref="decimal"/>.
+	/// </summary>
+	/// <param name="unit">Object <see cref="Unit"/>.</param>
+	/// <returns><see cref="decimal"/> value.</returns>
+	public static explicit operator decimal?(Unit unit)
+	{
+		if (unit is null)
+			return null;
+
+		return (decimal)unit;
+	}
+
+	/// <summary>
+	/// Cast <see cref="double"/> object to the type <see cref="Unit"/>.
+	/// </summary>
+	/// <param name="value"><see cref="double"/> value.</param>
+	/// <returns>Object <see cref="Unit"/>.</returns>
+	public static implicit operator Unit(double value) => (decimal)value;
+
+	/// <summary>
+	/// Cast object from <see cref="Unit"/> to <see cref="double"/>.
+	/// </summary>
+	/// <param name="unit">Object <see cref="Unit"/>.</param>
+	/// <returns><see cref="double"/> value.</returns>
+	public static explicit operator double(Unit unit)
+	{
+		if (unit is null)
+			throw new ArgumentNullException(nameof(unit));
+
+		return (double)(decimal)unit;
+	}
+
+	/// <summary>
+	/// Cast object from <see cref="Unit"/> to nullable <see cref="double"/>.
+	/// </summary>
+	/// <param name="unit">Object <see cref="Unit"/>.</param>
+	/// <returns><see cref="double"/> value.</returns>
+	public static explicit operator double?(Unit unit)
+	{
+		if (unit is null)
+			return null;
+
+		return (double)unit;
+	}
+
+	// for languages without auto Unit casting
+
+	/// <summary>
+	/// Add the two objects <see cref="Unit"/> and <see cref="decimal"/>.
+	/// </summary>
+	/// <param name="d">First object <see cref="decimal"/>.</param>
+	/// <param name="u">Second object <see cref="Unit"/>.</param>
+	/// <returns>The result of addition.</returns>
+	public static Unit operator +(decimal d, Unit u) => new Unit(d) + u;
+
+	/// <summary>
+	/// Add the two objects <see cref="Unit"/> and <see cref="decimal"/>.
+	/// </summary>
+	/// <param name="u">First object <see cref="Unit"/>.</param>
+	/// <param name="d">Second object <see cref="decimal"/>.</param>
+	/// <returns>The result of addition.</returns>
+	public static Unit operator +(Unit u, decimal d) => u + new Unit(d);
+
+	/// <summary>
+	/// Add the two objects <see cref="Unit"/> and <see cref="int"/>.
+	/// </summary>
+	/// <param name="i">First object <see cref="int"/>.</param>
+	/// <param name="u">Second object <see cref="Unit"/>.</param>
+	/// <returns>The result of addition.</returns>
+	public static Unit operator +(int i, Unit u) => new Unit(i) + u;
+
+	/// <summary>
+	/// Add the two objects <see cref="Unit"/> and <see cref="int"/>.
+	/// </summary>
+	/// <param name="u">First object <see cref="Unit"/>.</param>
+	/// <param name="i">Second object <see cref="int"/>.</param>
+	/// <returns>The result of addition.</returns>
+	public static Unit operator +(Unit u, int i) => u + new Unit(i);
+
+	/// <summary>
+	/// Subtract the <see cref="Unit"/> from a <see cref="decimal"/>.
+	/// </summary>
+	/// <param name="d">First object <see cref="decimal"/>.</param>
+	/// <param name="u">Second object <see cref="Unit"/>.</param>
+	/// <returns>The result of subtraction.</returns>
+	public static Unit operator -(decimal d, Unit u) => new Unit(d) - u;
+
+	/// <summary>
+	/// Subtract a <see cref="decimal"/> from a <see cref="Unit"/>.
+	/// </summary>
+	/// <param name="u">First object <see cref="Unit"/>.</param>
+	/// <param name="d">Second object <see cref="decimal"/>.</param>
+	/// <returns>The result of subtraction.</returns>
+	public static Unit operator -(Unit u, decimal d) => u - new Unit(d);
+
+	/// <summary>
+	/// Subtract the <see cref="Unit"/> from an <see cref="int"/>.
+	/// </summary>
+	/// <param name="i">First object <see cref="int"/>.</param>
+	/// <param name="u">Second object <see cref="Unit"/>.</param>
+	/// <returns>The result of subtraction.</returns>
+	public static Unit operator -(int i, Unit u) => new Unit(i) - u;
+
+	/// <summary>
+	/// Subtract an <see cref="int"/> from a <see cref="Unit"/>.
+	/// </summary>
+	/// <param name="u">First object <see cref="Unit"/>.</param>
+	/// <param name="i">Second object <see cref="int"/>.</param>
+	/// <returns>The result of subtraction.</returns>
+	public static Unit operator -(Unit u, int i) => u - new Unit(i);
+
+	/// <summary>
+	/// Multiply a <see cref="decimal"/> by a <see cref="Unit"/>.
+	/// </summary>
+	/// <param name="d">First object <see cref="decimal"/>.</param>
+	/// <param name="u">Second object <see cref="Unit"/>.</param>
+	/// <returns>The result of multiplication.</returns>
+	public static Unit operator *(decimal d, Unit u) => new Unit(d) * u;
+
+	/// <summary>
+	/// Multiply a <see cref="Unit"/> by a <see cref="decimal"/>.
+	/// </summary>
+	/// <param name="u">First object <see cref="Unit"/>.</param>
+	/// <param name="d">Second object <see cref="decimal"/>.</param>
+	/// <returns>The result of multiplication.</returns>
+	public static Unit operator *(Unit u, decimal d) => u * new Unit(d);
+
+	/// <summary>
+	/// Multiply an <see cref="int"/> by a <see cref="Unit"/>.
+	/// </summary>
+	/// <param name="i">First object <see cref="int"/>.</param>
+	/// <param name="u">Second object <see cref="Unit"/>.</param>
+	/// <returns>The result of multiplication.</returns>
+	public static Unit operator *(int i, Unit u) => new Unit(i) * u;
+
+	/// <summary>
+	/// Multiply a <see cref="Unit"/> by an <see cref="int"/>.
+	/// </summary>
+	/// <param name="u">First object <see cref="Unit"/>.</param>
+	/// <param name="i">Second object <see cref="int"/>.</param>
+	/// <returns>The result of multiplication.</returns>
+	public static Unit operator *(Unit u, int i) => u * new Unit(i);
+
+	/// <summary>
+	/// Divide a <see cref="decimal"/> by a <see cref="Unit"/>.
+	/// </summary>
+	/// <param name="d">First object <see cref="decimal"/>.</param>
+	/// <param name="u">Second object <see cref="Unit"/>.</param>
+	/// <returns>The result of division.</returns>
+	public static Unit operator /(decimal d, Unit u) => new Unit(d) / u;
+
+	/// <summary>
+	/// Divide a <see cref="Unit"/> by a <see cref="decimal"/>.
+	/// </summary>
+	/// <param name="u">First object <see cref="Unit"/>.</param>
+	/// <param name="d">Second object <see cref="decimal"/>.</param>
+	/// <returns>The result of division.</returns>
+	public static Unit operator /(Unit u, decimal d) => u / new Unit(d);
+
+	/// <summary>
+	/// Divide an <see cref="int"/> by a <see cref="Unit"/>.
+	/// </summary>
+	/// <param name="i">First object <see cref="int"/>.</param>
+	/// <param name="u">Second object <see cref="Unit"/>.</param>
+	/// <returns>The result of division.</returns>
+	public static Unit operator /(int i, Unit u) => new Unit(i) / u;
+
+	/// <summary>
+	/// Divide a <see cref="Unit"/> by an <see cref="int"/>.
+	/// </summary>
+	/// <param name="u">First object <see cref="Unit"/>.</param>
+	/// <param name="i">Second object <see cref="int"/>.</param>
+	/// <returns>The result of division.</returns>
+	public static Unit operator /(Unit u, int i) => u / new Unit(i);
+}
