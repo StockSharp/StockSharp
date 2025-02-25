511409ec523ae0bffa735344c6dc64648ceb173e
mikasoukhov <mika@stocksharp.com>
2025-02-25 18:04:29 +0300

    Fix commit 51aaa67879768d69e8ae6c76a04714170b5c4542

--- Algo/Indicators/LinearRegRSquared.cs
+++ Algo/Indicators/LinearRegRSquared.cs
@@ -4,14 +4,13 @@ namespace StockSharp.Algo.Indicators;
 /// Linear regression R-squared.
 /// </summary>
 /// <remarks>
-/// https://doc.stocksharp.com/topics/IndicatorLinearRegRSquared.html
+/// https://doc.stocksharp.com/topics/api/indicators/list_of_indicators/linear_reg_rsquared.html
 /// </remarks>
-//[Display(
-//	ResourceType = typeof(LocalizedStrings),
-//	Name = LocalizedStrings.RSquaredKey,
-//	Description = LocalizedStrings.LinearRegRSquaredKey)]
-//[Doc("topics/IndicatorLinearRegRSquared.html")]
-[Browsable(false)]
+[Display(
+	ResourceType = typeof(LocalizedStrings),
+	Name = LocalizedStrings.RSquaredKey,
+	Description = LocalizedStrings.LinearRegRSquaredKey)]
+[Doc("topics/api/indicators/list_of_indicators/linear_reg_rsquared.html")]
 public class LinearRegRSquared : LengthIndicator<decimal>
 {
 	// Коэффициент при независимой переменной, угол наклона прямой.
