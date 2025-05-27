a223c11d693c106817211238fd3c228b4fa6e14e
mikasoukhov <mika@stocksharp.com>
2025-05-27 15:09:15 +0300

    ITimeProvider. Removed inheritance from ILogSource.

--- BusinessEntities/ITimeProvider.cs
+++ BusinessEntities/ITimeProvider.cs
@@ -3,10 +3,15 @@
 /// <summary>
 /// The interface describing the time provider.
 /// </summary>
-public interface ITimeProvider : ILogSource
+public interface ITimeProvider
 {
 	/// <summary>
-	/// Server time changed <see cref="ILogSource.CurrentTime"/>. It passed the time difference since the last call of the event. The first time the event passes the value <see cref="TimeSpan.Zero"/>.
+	/// Current time.
+	/// </summary>
+	DateTimeOffset CurrentTime { get; }
+
+	/// <summary>
+	/// Server time changed <see cref="CurrentTime"/>. It passed the time difference since the last call of the event. The first time the event passes the value <see cref="TimeSpan.Zero"/>.
 	/// </summary>
 	event Action<TimeSpan> CurrentTimeChanged;
 }
