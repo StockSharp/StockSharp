90fddebcf813c5ad72618f892c0d9007365f4730
mikasoukhov <mika@stocksharp.com>
2025-06-03 00:35:28 +0300

    Fix commit bd1136f387133a428901475faea5a4d41b65dab1

---
+++ Media.Names/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("S#.Media.Names")]
+[assembly: AssemblyDescription("Media names components.")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("FA8762A7-591F-4158-B790-4AE30B0478D7")]
