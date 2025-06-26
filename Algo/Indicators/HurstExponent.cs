1bc60ee04f9f819e5edc43dfd36d037ab4526b90
mikasoukhov <mika@stocksharp.com>
2025-06-26 23:02:41 +0300

    HurstExponent indicator.

---
+++ Algo/Indicators/HurstExponent.cs
@@ -0,0 +1,71 @@
+﻿namespace StockSharp.Algo.Indicators;
+
+/// <summary>
+/// Hurst Exponent indicator.
+/// </summary>
+/// <remarks>
+/// Measures the tendency of a time series to regress strongly to the mean or to cluster in a direction.
+/// https://en.wikipedia.org/wiki/Hurst_exponent
+/// </remarks>
+[Display(
+	ResourceType = typeof(LocalizedStrings),
+	Name = LocalizedStrings.HurstExponentKey,
+	Description = LocalizedStrings.HurstExponentDescKey)]
+public class HurstExponent : LengthIndicator<decimal>
+{
+	/// <summary>
+	/// Initializes a new instance of the <see cref="HurstExponent"/> class.
+	/// </summary>
+	public HurstExponent()
+	{
+		Length = 100;
+	}
+
+	/// <inheritdoc />
+	public override IndicatorMeasures Measure => IndicatorMeasures.MinusOnePlusOne;
+
+	/// <inheritdoc />
+	protected override decimal? OnProcessDecimal(IIndicatorValue input)
+	{
+		if (input.IsFinal)
+			Buffer.PushBack(input.ToDecimal());
+
+		if (!IsFormed)
+			return null;
+
+		var values = Buffer.Select(x => x).ToArray();
+
+		var n = values.Length;
+
+		var mean = values.Average();
+
+		var dev = new decimal[n];
+		var cumdev = new decimal[n];
+
+		for (var i = 0; i < n; i++)
+			dev[i] = values[i] - mean;
+
+		cumdev[0] = dev[0];
+
+		for (var i = 1; i < n; i++)
+			cumdev[i] = cumdev[i - 1] + dev[i];
+
+		var maxCum = cumdev.Max();
+		var minCum = cumdev.Min();
+		var range = maxCum - minCum;
+
+		var sumSqr = 0m;
+		for (var i = 0; i < n; i++)
+			sumSqr += (values[i] - mean) * (values[i] - mean);
+
+		var std = (decimal)Math.Sqrt((double)(sumSqr / n));
+
+		if (std == 0)
+			return null;
+
+		var RS = range / std;
+		var H = RS.Log() / ((decimal)n).Log();
+
+		return H;
+	}
+}
