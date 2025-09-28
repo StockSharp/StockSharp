94544b594dda29b50734245a29a283dbd34307de
mikasoukhov <mika@stocksharp.com>
2025-09-28 22:37:03 +0300

    BullPower

---
+++ Algo/Indicators/BullPower.cs
@@ -0,0 +1,54 @@
+namespace StockSharp.Algo.Indicators;
+
+/// <summary>
+/// Bull Power indicator.
+/// </summary>
+/// <remarks>
+/// https://doc.stocksharp.com/topics/api/indicators/list_of_indicators/bull_power.html
+/// </remarks>
+[Display(
+	ResourceType = typeof(LocalizedStrings),
+	Name = LocalizedStrings.BullPowerKey,
+	Description = LocalizedStrings.BullPowerDescKey)]
+[IndicatorIn(typeof(CandleIndicatorValue))]
+[Doc("topics/api/indicators/list_of_indicators/bull_power.html")]
+public class BullPower : LengthIndicator<decimal>
+{
+	private readonly ExponentialMovingAverage _ema;
+
+	/// <summary>
+	/// Initializes a new instance of the <see cref="BullPower"/>.
+	/// </summary>
+	public BullPower()
+	{
+		_ema = new() { Length = 13 };
+	}
+
+	/// <inheritdoc />
+	public override IndicatorMeasures Measure => IndicatorMeasures.Percent;
+
+	/// <inheritdoc />
+	public override int NumValuesToInitialize => _ema.NumValuesToInitialize;
+
+	/// <inheritdoc />
+	protected override bool CalcIsFormed() => _ema.IsFormed;
+
+	/// <inheritdoc />
+	protected override decimal? OnProcessDecimal(IIndicatorValue input)
+	{
+		var candle = input.ToCandle();
+		var emaValue = _ema.Process(input);
+
+		if (_ema.IsFormed && !emaValue.IsEmpty)
+			return candle.HighPrice - emaValue.ToDecimal();
+
+		return null;
+	}
+
+	/// <inheritdoc />
+	public override void Reset()
+	{
+		_ema.Length = Length;
+		base.Reset();
+	}
+}
