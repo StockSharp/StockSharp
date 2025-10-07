d193f0b1177f356433c7491796ed45616a06dbcb
mikasoukhov <mika@stocksharp.com>
2025-10-07 22:47:24 +0300

    BatchEmulation removed as obsolete.

--- Algo/Strategies/Optimization/OptimizerSettings.cs
+++ Algo/Strategies/Optimization/OptimizerSettings.cs
@@ -7,40 +7,6 @@ using StockSharp.Algo.Testing;
 /// </summary>
 public class OptimizerSettings : MarketEmulatorSettings
 {
-	private DateTime _startTime = DateTime.Today.AddYears(-1);
-
-	/// <summary>
-	/// Date in history for starting the paper trading.
-	/// </summary>
-	[Browsable(false)]
-	[Obsolete("Use specified parameters in Start methods.")]
-	public DateTime StartTime
-	{
-		get => _startTime;
-		set
-		{
-			_startTime = value;
-			NotifyPropertyChanged();
-		}
-	}
-
-	private DateTime _stopTime = DateTime.Today;
-
-	/// <summary>
-	/// Date in history to stop the paper trading (date is included).
-	/// </summary>
-	[Browsable(false)]
-	[Obsolete("Use specified parameters in Start methods.")]
-	public DateTime StopTime
-	{
-		get => _stopTime;
-		set
-		{
-			_stopTime = value;
-			NotifyPropertyChanged();
-		}
-	}
-
 	private int _batchSize = Environment.ProcessorCount * 2;
 
 	/// <summary>
@@ -106,10 +72,6 @@ public class OptimizerSettings : MarketEmulatorSettings
 		base.Save(storage);
 
 		storage
-#pragma warning disable CS0618 // Type or member is obsolete
-			.Set(nameof(StartTime), StartTime)
-			.Set(nameof(StopTime), StopTime)
-#pragma warning restore CS0618 // Type or member is obsolete
 			.Set(nameof(BatchSize), BatchSize)
 			.Set(nameof(MaxIterations), MaxIterations)
 			.Set(nameof(MaxMessageCount), MaxMessageCount)
@@ -121,10 +83,6 @@ public class OptimizerSettings : MarketEmulatorSettings
 	{
 		base.Load(storage);
 
-#pragma warning disable CS0618 // Type or member is obsolete
-		StartTime = storage.GetValue(nameof(StartTime), StartTime);
-		StopTime = storage.GetValue(nameof(StopTime), StopTime);
-#pragma warning restore CS0618 // Type or member is obsolete
 		BatchSize = storage.GetValue(nameof(BatchSize), BatchSize);
 		MaxIterations = storage.GetValue(nameof(MaxIterations), MaxIterations);
 		MaxMessageCount = storage.GetValue(nameof(MaxMessageCount), MaxMessageCount);
