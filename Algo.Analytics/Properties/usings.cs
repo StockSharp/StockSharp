34be1ec81bc456256ce1de075fb1c2a42b14cb13
mikasoukhov <mika@stocksharp.com>
2025-12-19 23:00:23 +0300

    Code cleanup.

--- Algo.Analytics/Properties/usings.cs
+++ Algo.Analytics/Properties/usings.cs
@@ -1,11 +1,9 @@
 global using global::System;
 global using global::System.Collections.Generic;
-global using global::System.Linq;
 global using global::System.Threading;
 global using global::System.Threading.Tasks;
 global using global::System.Drawing;
 
-global using global::Ecng.Common;
 global using global::Ecng.Drawing;
 global using global::Ecng.Logging;
 
