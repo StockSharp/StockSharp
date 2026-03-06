1c0e8bbb0a364c2841d2474a18c15591d148a6a0
mika@stocksharp.com <mika@stocksharp.com>
2026-03-06 21:38:57 +0300

    Add missing AssemblyInfo.cs for MatchingEngine

---
+++ MatchingEngine/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("S#.MatchingEngine")]
+[assembly: AssemblyDescription("Order matching engine for backtesting and paper trading.")]
+
+[assembly: Guid("e91c1db8-7221-4a8f-9071-5b3d6d081754")]
